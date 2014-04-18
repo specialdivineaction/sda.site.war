fc7f556f554815264ea44a6263a1abf7009eccc5
Neal Audenaert <neala@tamu.edu>
2014-04-18 15:59:42 -0500

    Refactored PsqlWorkRepo to use initial db executor framework.
    https://issues.citd.tamu.edu/browse/CE-2

--- bundles/edu.tamu.tcat.sda.catalog.psql/src/edu/tamu/tcat/sda/catalog/psql/PsqlWorkRepo.java
+++ bundles/edu.tamu.tcat.sda.catalog.psql/src/edu/tamu/tcat/sda/catalog/psql/PsqlWorkRepo.java
@@ -1,15 +1,17 @@
 package edu.tamu.tcat.sda.catalog.psql;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.util.List;
 
 import org.postgresql.util.PGobject;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
+import edu.tamu.tcat.oss.db.DbExecTask;
+import edu.tamu.tcat.oss.db.DbExecutor;
+import edu.tamu.tcat.oss.json.JsonException;
+import edu.tamu.tcat.oss.json.JsonMapper;
+import edu.tamu.tcat.sda.catalog.works.AuthorReference;
+import edu.tamu.tcat.sda.catalog.works.Title;
 import edu.tamu.tcat.sda.catalog.works.Work;
 import edu.tamu.tcat.sda.catalog.works.WorkRepository;
 import edu.tamu.tcat.sda.catalog.works.dv.WorkDV;
@@ -19,6 +21,15 @@ public class PsqlWorkRepo implements WorkRepository
 {
    // TODO should we use something like the data source executor service?
 
+   private final DbExecutor exec;
+   private final JsonMapper jsonMapper;
+
+   public PsqlWorkRepo(DbExecutor exec, JsonMapper jsonMapper)
+   {
+      this.exec = exec;
+      this.jsonMapper = jsonMapper;
+   }
+
    @Override
    public Iterable<Work> listWorks()
    {
@@ -26,69 +37,140 @@ public class PsqlWorkRepo implements WorkRepository
       return null;
    }
 
-	@Override
-	public void create(WorkDV work, DataUpdateObserver<WorkDV, Work> observer) 
-	{
-	   
-	      String sql = "INSERT INTO works (id, work) VALUES(?,?)";
-	   	ObjectMapper workMap = new ObjectMapper();
-	   	try
-         {
-	   	   Connection conn = getPostgresConn();
-            String workString = workMap.writeValueAsString(work);
-            
-            PGobject jsonObject = new PGobject();
-            jsonObject.setType("json");
-            jsonObject.setValue(workString);
-            
-            PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setString(1, work.id);
-            ps.setObject(2, jsonObject);
-            
-            int complete = ps.executeUpdate();
-            if (complete == 1)
-               conn.close();
-         }
-         catch (JsonProcessingException e)
-         {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-         }
-         catch (SQLException e)
-         {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-         }
-	}
+   @Override
+   public void create(final WorkDV work, final DataUpdateObserver<WorkDV, Work> observer)
+   {
+//      ObjectMapper workMap = new ObjectMapper();
+      final String workString;
+      try
+      {
+         // TODO use a supplied object mapper
+         workString = jsonMapper.asString(work);
+      }
+      catch (JsonException jpe)
+      {
+         throw new IllegalArgumentException("Failed to serialize the supplied work [" + work + "]", jpe);
+      }
 
-	@Override
-	public void update(WorkDV work, DataUpdateObserver<WorkDV, Work> observer) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	private Connection getPostgresConn()
-	{
-		Connection con = null;
+      final String sql = "INSERT INTO works (id, work) VALUES(?,?)";
+      exec.submit(new DbExecTask()
+      {
+         private Connection conn;
 
-    
-        String url = "jdbc:postgresql://localhost:5433/SDA";
-        String user = "postgres";
-        String password = "";
-        
-        try 
-        {
-           
-         Class.forName("org.postgresql.Driver");
-			con = DriverManager.getConnection(url, user, password);
-			
-		  } 
-        catch (SQLException | ClassNotFoundException e) 
-        {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        return con;
-	}
+         @Override
+         public void run()
+         {
+            if (observer.isCanceled())
+            {
+               observer.onAborted();
+               return;
+            }
+
+            observer.onStart(); // notify observer that we are about to start
+            try (PreparedStatement ps = conn.prepareStatement(sql))
+            {
+               PGobject jsonObject = new PGobject();
+               jsonObject.setType("json");
+               jsonObject.setValue(workString);
+
+               ps.setString(1, work.id);
+               ps.setObject(2, jsonObject);
+
+               int ct = ps.executeUpdate();
+               if (ct != 1)
+                  throw new IllegalStateException("Failed to create work. Unexpected number of rows updates [" + ct + "]");
+
+               Work w = new WorkRef(Long.parseLong(work.id));
+               observer.onFinish(w);
+            }
+            catch (Exception e)
+            {
+               observer.onError("Failed to save work: " + work, e);
+            }
+
+            return;
+         }
+
+         @Override
+         public void setConnection(Connection conn)
+         {
+            this.conn = conn;
+         }
+
+      });
+   }
+
+   @Override
+   public void update(WorkDV work, DataUpdateObserver<WorkDV, Work> observer)
+   {
+      // TODO Auto-generated method stub
+
+   }
+
+   private static class WorkRef implements Work
+   {
+      public WorkRef(long id)
+      {
+         // TODO implement this. Capture the id of the work, use it to query the DB as 
+         //      needed to lazy load data.
+      }
+
+      @Override
+      public List<AuthorReference> getAuthors()
+      {
+         // TODO Auto-generated method stub
+         return null;
+      }
+
+      @Override
+      public Title getTitle()
+      {
+         // TODO Auto-generated method stub
+         return null;
+      }
+
+      @Override
+      public String getSeries()
+      {
+         // TODO Auto-generated method stub
+         return null;
+      }
+
+      @Override
+      public String getSummary()
+      {
+         // TODO Auto-generated method stub
+         return null;
+      }
+
+      @Override
+      public List<String> getNotes()
+      {
+         // TODO Auto-generated method stub
+         return null;
+      }
+
+   }
+
+//   private Connection getPostgresConn()
+//   {
+//      Connection con = null;
+//
+//      String url = "jdbc:postgresql://localhost:5433/SDA";
+//      String user = "postgres";
+//      String password = "";
+//
+//      try
+//      {
+//         Class.forName("org.postgresql.Driver");
+//         con = DriverManager.getConnection(url, user, password);
+//      }
+//      catch (SQLException | ClassNotFoundException e)
+//      {
+//         // TODO Auto-generated catch block
+//         e.printStackTrace();
+//      }
+//      return con;
+//   }
 
 }
--- bundles/edu.tamu.tcat.sda.catalog.psql/src/edu/tamu/tcat/sda/catalog/psql/tests/TestCreateWork.java
+++ bundles/edu.tamu.tcat.sda.catalog.psql/src/edu/tamu/tcat/sda/catalog/psql/tests/TestCreateWork.java
@@ -1,13 +1,23 @@
 package edu.tamu.tcat.sda.catalog.psql.tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import edu.tamu.tcat.oss.db.psql.DataSourceFactory;
+import edu.tamu.tcat.oss.db.psql.PsqlDbExec;
+import edu.tamu.tcat.oss.json.JsonException;
+import edu.tamu.tcat.oss.json.JsonMapper;
 import edu.tamu.tcat.sda.catalog.psql.PsqlWorkRepo;
 import edu.tamu.tcat.sda.catalog.works.Work;
 import edu.tamu.tcat.sda.catalog.works.dv.AuthorRefDv;
@@ -16,7 +26,29 @@ import edu.tamu.tcat.sda.ds.DataUpdateObserver;
 
 public class TestCreateWork 
 {
+   private PsqlDbExec dbExec;
 
+   @BeforeClass
+   public void initDbConnection()
+   {
+      // TODO make configurable
+      // FIXME at the moment, we have no way to clean up after tests!!
+      String url = "jdbc:postgresql://localhost:5433/SDA";
+      String user = "postgres";
+      String pass = "";
+      
+      
+      DataSourceFactory factory = new DataSourceFactory();
+      dbExec = new PsqlDbExec(factory.getDataSource(url, user, pass));
+   }
+   
+   @AfterClass
+   public void tearDown() 
+   {
+      if (dbExec != null)
+         dbExec.close();
+   }
+   
 	@Test
 	public void testCreate() 
 	{
@@ -33,7 +65,7 @@ public class TestCreateWork
 		works.id = UUID.randomUUID().toString();
 		works.authors = authorList;
 		
-		PsqlWorkRepo workRepo = new PsqlWorkRepo();
+		PsqlWorkRepo workRepo = new PsqlWorkRepo(dbExec, new SimpleJacksonMapper());
 		DataUpdateObserver<WorkDV, Work> observer = null;
       workRepo.create(works, observer);
 		
@@ -41,4 +73,35 @@ public class TestCreateWork
 		fail("Not yet implemented");
 	}
 
+	private static class SimpleJacksonMapper implements JsonMapper
+	{
+	   ObjectMapper mapper = new ObjectMapper();
+	   
+      @Override
+      public String asString(Object o) throws JsonException
+      {
+         try
+         {
+            return mapper.writeValueAsString(o);
+         }
+         catch (JsonProcessingException e)
+         {
+            throw new JsonException(e);
+         }
+      }
+      
+      @Override
+      public <T> T parse(String json, Class<T> type) throws JsonException
+      {
+         try
+         {
+            return mapper.readValue(json, type);
+         }
+         catch (IOException e)
+         {
+            throw new JsonException(e);
+         }
+      }
+	   
+	}
 }
