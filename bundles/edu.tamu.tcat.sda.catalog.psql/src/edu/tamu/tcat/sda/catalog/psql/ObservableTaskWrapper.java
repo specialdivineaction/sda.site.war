8c7c758cfb80f5902eda17a694cf1318ca7c4e3e
Neal Audenaert <neala@tamu.edu>
2014-04-21 10:43:35 -0500

    Improved exception handling in ObservableTaskWrapper
    https://issues.citd.tamu.edu/browse/CE-2

--- bundles/edu.tamu.tcat.sda.catalog.psql/src/edu/tamu/tcat/sda/catalog/psql/ObservableTaskWrapper.java
+++ bundles/edu.tamu.tcat.sda.catalog.psql/src/edu/tamu/tcat/sda/catalog/psql/ObservableTaskWrapper.java
@@ -1,6 +1,5 @@
 package edu.tamu.tcat.sda.catalog.psql;
 
-import java.nio.channels.IllegalSelectorException;
 import java.sql.Connection;
 
 import edu.tamu.tcat.oss.db.DbExecTask;
@@ -17,22 +16,43 @@ public class ObservableTaskWrapper<R> implements DbExecTask<R>
       this.observer = observer;
    }
 
+   /**
+    * In the event that either the wrapped task or the observer throw an exception, this
+    * will attempt to notify the observer and propagate the exception. Since tasks will 
+    * typically be run in an {@code Executor}, this make the exception available to clients 
+    * via the Java concurrency API's {@code Future} interface.
+    * 
+    * <p>
+    * Note that this may return null if the task is canceled by the observer or if the 
+    * underlying task returns null.
+    */
    @Override
-   public R execute(Connection conn)
+   public R execute(Connection conn) throws Exception
    {
-      if (conn == null)
+      try 
       {
-         String msg = "Cannot execute database task. No connection supplied.";
-         observer.error(msg, new IllegalSelectorException());
-      }
-      
-      if (observer.isCanceled())
-      {
-         observer.aborted();
-         return null;
-      }
+         checkConnection(conn);
 
-      observer.start(); // notify observer that we are about to start
+         if (observer.isCanceled())
+         {
+            observer.aborted();
+            return null;
+         }
+
+         observer.start();
+         return runTask(conn);
+      } 
+      catch(Exception ex)
+      {
+         if (!observer.isCompleted())
+            observer.error(ex.getMessage(), ex);
+         
+         throw ex;
+      }
+   }
+
+   private R runTask(Connection conn) throws Exception
+   {
       try 
       {
          R result = task.execute(conn);
@@ -42,8 +62,18 @@ public class ObservableTaskWrapper<R> implements DbExecTask<R>
       catch (Exception e)
       {
          observer.error(e.getMessage(), e);
+         throw e;    // TODO should provide wrapper for this exception
       }
-      
-      return null;
+   }
+
+   private void checkConnection(Connection conn)
+   {
+      if (conn == null)
+      {
+         String msg = "Cannot execute database task. No connection supplied.";
+         IllegalStateException ex = new IllegalStateException(msg);
+         observer.error(msg, ex);
+         throw ex;
+      }
    }
 }
