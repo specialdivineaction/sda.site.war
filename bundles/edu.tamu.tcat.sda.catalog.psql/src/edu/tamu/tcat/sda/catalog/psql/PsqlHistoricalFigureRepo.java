070a7e1d64dc8a3dc4caa1be2b2720e82aaef206
Jesse Mitchell <jessemitchell@tees.tamus.edu>
2014-05-13 11:09:17 -0500

    HistoricalFigure Repository sql code
    https://issues.citd.tamu.edu/browse/CE-4

--- bundles/edu.tamu.tcat.sda.catalog.psql/src/edu/tamu/tcat/sda/catalog/psql/PsqlHistoricalFigureRepo.java
+++ bundles/edu.tamu.tcat.sda.catalog.psql/src/edu/tamu/tcat/sda/catalog/psql/PsqlHistoricalFigureRepo.java
@@ -1,7 +1,18 @@
 package edu.tamu.tcat.sda.catalog.psql;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Set;
 
+import org.postgresql.util.PGobject;
+
+import edu.tamu.tcat.oss.db.DbExecTask;
+import edu.tamu.tcat.oss.db.DbExecutor;
+import edu.tamu.tcat.oss.json.JsonException;
+import edu.tamu.tcat.oss.json.JsonMapper;
 import edu.tamu.tcat.sda.catalog.events.HistoricalEvent;
 import edu.tamu.tcat.sda.catalog.people.HistoricalFigure;
 import edu.tamu.tcat.sda.catalog.people.HistoricalFigureRepository;
@@ -11,6 +22,15 @@ import edu.tamu.tcat.sda.ds.DataUpdateObserver;
 
 public class PsqlHistoricalFigureRepo implements HistoricalFigureRepository
 {
+   
+   private final DbExecutor exec;
+   private final JsonMapper jsonMapper;
+
+   public PsqlHistoricalFigureRepo(DbExecutor exec, JsonMapper jsonMapper)
+   {
+      this.exec = exec;
+      this.jsonMapper = jsonMapper;
+   }
 
    @Override
    public Iterable<HistoricalFigure> listHistoricalFigures()
@@ -20,21 +40,103 @@ public class PsqlHistoricalFigureRepo implements HistoricalFigureRepository
    }
 
    @Override
-   public void create(HistoricalFigureDV histFigure, DataUpdateObserver<HistoricalFigure> observer)
+   public void create(final HistoricalFigureDV histFigure, DataUpdateObserver<HistoricalFigure> observer)
    {
-      // TODO Auto-generated method stub
+      final String insertSql = "INSERT INTO people (historical_figure) VALUES(null)";
+      final String updateSql = "UPDATE people "
+                               + " SET historical_figure = ?"
+                               + " WHERE id = ?";
+      DbExecTask<HistoricalFigure> task1 = new DbExecTask<HistoricalFigure>()
+      {
+         @Override
+         public HistoricalFigure execute(Connection conn) throws SQLException
+         {
+            try (PreparedStatement ps = conn.prepareStatement(insertSql, Statement.RETURN_GENERATED_KEYS))
+            {
+
+               int ct = ps.executeUpdate();
+               if (ct != 1)
+                  throw new IllegalStateException("Failed to create historical figure. Unexpected number of rows updates [" + ct + "]");
+               else
+               {
+                  ResultSet rs = ps.getGeneratedKeys();
+                  if ( rs.next() )
+                  {
+                      int key = rs.getInt(1);
+                      histFigure.id = new Integer(key).toString();
+                  }
+               }
+            }
+            
+            try (PreparedStatement ps = conn.prepareStatement(updateSql))
+            {
+               PGobject jsonObject = new PGobject();
+               jsonObject.setType("json");
+               jsonObject.setValue(jsonMapper.asString(histFigure));
+
+               ps.setObject(1, jsonObject);
+               ps.setInt(2, Integer.parseInt(histFigure.id));
+
+               int ct = ps.executeUpdate();
+               if (ct != 1)
+                  throw new IllegalStateException("Failed to create historical figure. Unexpected number of rows updates [" + ct + "]");
+
+            }
+            catch (JsonException e)
+            {
+               throw new IllegalArgumentException("Failed to serialize the supplied historical figure [" + histFigure + "]", e);
+            }
+            return new HistoricalFigureRef(Long.parseLong(histFigure.id));
+         }
+      };
+      
+      exec.submit(new ObservableTaskWrapper<>(task1, observer));
       
    }
 
    @Override
-   public void update(HistoricalFigureDV histFigure, DataUpdateObserver<HistoricalFigure> observer)
+   public void update(final HistoricalFigureDV histFigure, DataUpdateObserver<HistoricalFigure> observer)
    {
-      // TODO Auto-generated method stub
+      final String updateSql = "UPDATE people "
+            + " SET historical_figure = ?"
+            + " WHERE id = ?";
+      DbExecTask<HistoricalFigure> task1 = new DbExecTask<HistoricalFigure>()
+      {
+         @Override
+         public HistoricalFigure execute(Connection conn) throws SQLException
+         {
+            try (PreparedStatement ps = conn.prepareStatement(updateSql))
+            {
+               PGobject jsonObject = new PGobject();
+               jsonObject.setType("json");
+               jsonObject.setValue(jsonMapper.asString(histFigure));
+               
+               ps.setObject(1, jsonObject);
+               ps.setInt(2, Integer.parseInt(histFigure.id));
+               
+               int ct = ps.executeUpdate();
+               if (ct != 1)
+               throw new IllegalStateException("Failed to create historical figure. Unexpected number of rows updates [" + ct + "]");
+               
+            }
+            catch (JsonException e)
+            {
+               throw new IllegalArgumentException("Failed to serialize the supplied historical figure [" + histFigure + "]", e);
+            }
+            return new HistoricalFigureRef(Long.parseLong(histFigure.id));
+         }
+      };
+      
+      exec.submit(new ObservableTaskWrapper<>(task1, observer));      // TODO Auto-generated method stub
       
    }
 
    private static class HistoricalFigureRef implements HistoricalFigure
    {
+      public HistoricalFigureRef(long id)
+      {
+         
+      }
 
       @Override
       public String getId()
