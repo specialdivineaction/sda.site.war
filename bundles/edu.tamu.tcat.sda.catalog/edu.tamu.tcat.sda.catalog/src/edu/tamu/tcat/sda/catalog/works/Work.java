a9f22a951944b9cec6ea8ba2dd1510412e883889
Neal Audenaert <neala@tamu.edu>
2014-05-15 11:14:59 -0500

    Created AuthorList, added PubInfo, #otherAuthors to work API.
    https://issues.citd.tamu.edu/browse/CE-4
    
    Conflicts:
    	bundles/edu.tamu.tcat.sda.catalog.psql/src/edu/tamu/tcat/sda/catalog/psql/PsqlWorkRepo.java

--- bundles/edu.tamu.tcat.sda.catalog/edu.tamu.tcat.sda.catalog/src/edu/tamu/tcat/sda/catalog/works/Work.java
+++ bundles/edu.tamu.tcat.sda.catalog/edu.tamu.tcat.sda.catalog/src/edu/tamu/tcat/sda/catalog/works/Work.java
@@ -1,6 +1,5 @@
 package edu.tamu.tcat.sda.catalog.works;
 
-import java.util.List;
 
 /**
  * Bibliographic description for a book, article, journal or other work. This is the main 
@@ -11,12 +10,26 @@ public interface Work
    /**
     * @return The authors of this work. 
     */
-   List<AuthorReference> getAuthors();    // TODO create AuthorList type
+   AuthorList getAuthors();   
    
    /**
     * @return The title of this work.
     */
-   Title getTitle();
+   TitleDefinition getTitle();
+   
+   /**
+    * @return Secondary authors associated with this work. This corresponds to authors that 
+    *    would typically be displayed after the title information, such as the translator of a 
+    *    work. For example, in the entry Spinoza. <em>Tractatus Theologico-Politicus</em>. 
+    *    Trans by Willis. 1862. Willis would be the 'outher authors'.
+    *  
+    */
+   AuthorList getOtherAuthors();  
+   
+   /**
+    * @return Details about when, where and by whom this work was published.
+    */
+   PublicationInfo getPublicationInfo();
    
    /**
     * @return A defined series of related works, typically published by a single publishers and 
