1c04768b2c77ddd34c0f8cf91b2e5102ff501c31
Neal Audenaert <neala@tamu.edu>
2014-05-27 11:59:29 -0500

    Added some JavaDoc to HistoricFigureRepo API.
    https://issues.citd.tamu.edu/browse/CE-4

--- bundles/edu.tamu.tcat.sda.catalog/edu.tamu.tcat.sda.catalog/src/edu/tamu/tcat/sda/catalog/people/HistoricalFigureRepository.java
+++ bundles/edu.tamu.tcat.sda.catalog/edu.tamu.tcat.sda.catalog/src/edu/tamu/tcat/sda/catalog/people/HistoricalFigureRepository.java
@@ -3,12 +3,29 @@ package edu.tamu.tcat.sda.catalog.people;
 import edu.tamu.tcat.sda.catalog.people.dv.HistoricalFigureDV;
 import edu.tamu.tcat.sda.datastore.DataUpdateObserver;
 
+/**
+ * 
+ *
+ */
 public interface HistoricalFigureRepository
 {
    Iterable<HistoricalFigure> listHistoricalFigures();
    
    HistoricalFigure getPerson(long personId);
    
+   /**
+    * Creates a new entry for the supplied historical figure. Note that no de-duplication will
+    * be performed. If this person (or a similar person) has already been added, a new entry
+    * will be created.
+    * 
+    * <p>This method will execute asynchronously. Upon success, it will pass an instance of 
+    * {@link HistoricalFigure} representing the create person to the observer. On failure, it 
+    * will supply an error message and optionally, a exception associated with the failure. 
+    * 
+    * @param histFigure A data vehicle containing the information for the person to create. 
+    * @param observer An optional observer that will be notified upon success or failure of 
+    *       this operation.
+    */
    void create(HistoricalFigureDV histFigure, DataUpdateObserver<HistoricalFigure> observer);
    
    void update(HistoricalFigureDV histFigure, DataUpdateObserver<HistoricalFigure> observer);
