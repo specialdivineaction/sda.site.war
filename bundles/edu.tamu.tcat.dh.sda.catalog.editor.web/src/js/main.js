084348ef14e7153b0ecbd3ad6807cb5baedfed51
Matthew Barry <mbarry@tamu.edu>
2014-06-13 13:44:04 -0500

    Basic mock-up demonstrating desired form behavior
    https://issues.citd.tamu.edu/browse/CE-12
    
    1. Author information popup next to Author field. with option to edit
    2. Author Quick-Add Modal dialog.

--- bundles/edu.tamu.tcat.dh.sda.catalog.editor.web/src/js/main.js
+++ bundles/edu.tamu.tcat.dh.sda.catalog.editor.web/src/js/main.js
@@ -1,3 +1,14 @@
 define(['jquery', 'bootstrap', 'jquery.autosize'], function ($) {
     $('textarea.autosize').autosize();
+
+    $('input#author')
+        .popover({
+            container: 'body',
+            html: true,
+            placement: 'left',
+            trigger: 'focus',
+            title: 'Author Details',
+            content: '<p><strong>Author:</strong> John Doe (1234-1320)<br/><strong>Display as:</strong> J. Doe<br/><strong>Role:</strong> Author</p><a href="#" class="btn btn-sm btn-block btn-info" data-toggle="modal" data-target="#quickAddForm">Edit</a>'
+        });
+
 });
