4c8d0b197a6dfbcc8b672248d93b217406d8d883
Neal Audenaert <nealaudenaert@tees.tamu.edu>
2014-06-16 14:13:35 -0500

    Added JavaDoc to DataUpdateObserverAdapter; deleted legacy test
    https://issues.citd.tamu.edu/browse/CE-4

--- bundles/edu.tamu.tcat.sda.catalog/edu.tamu.tcat.sda.catalog/src/edu/tamu/tcat/sda/datastore/DataUpdateObserverAdapter.java
+++ bundles/edu.tamu.tcat.sda.catalog/edu.tamu.tcat.sda.catalog/src/edu/tamu/tcat/sda/datastore/DataUpdateObserverAdapter.java
@@ -3,17 +3,32 @@ package edu.tamu.tcat.sda.datastore;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * Provides a base implementation of the {@link DataUpdateObserver} that implements the
+ * main API methods in order to maintain the state contract of the API. The methods delegate
+ * to {@code on*} methods (e.g., {@link #onFinish(Object)}) to allow extensions to handle
+ * events in the lifecycle of a database task. Default no-op implementations are provided for
+ * these methods to allow sub-classes to selectively override the methods they are interested in.
+ *
+ * @param <R> The result type of the database task.
+ */
+
 public class DataUpdateObserverAdapter<R> implements DataUpdateObserver<R>
 {
    private static final Logger logger = Logger.getLogger("org.tamu.tcat.sda.datastore.errors");
-   
+
    private volatile boolean cancelled = false;
    private volatile State state = State.PENDING;
-   
+
    public DataUpdateObserverAdapter()
    {
    }
 
+
+   /**
+    * Implements the {@link #start()} method to maintain the state contract of the interface
+    * and delegates event handling to the {@link #onStart()} method.
+    */
    @Override
    public final boolean start()
    {
@@ -21,8 +36,8 @@ public class DataUpdateObserverAdapter<R> implements DataUpdateObserver<R>
       {
          if (isCompleted())
             throw new IllegalStateException();
-         
-         try 
+
+         try
          {
             if (this.onStart())
             {
@@ -30,19 +45,26 @@ public class DataUpdateObserverAdapter<R> implements DataUpdateObserver<R>
                return true;
             }
          }
-         catch (Exception e) 
+         catch (Exception e)
          {
             logger.log(Level.SEVERE, "DataUpdateAdapter error: onStart failed [" + this + "]", e);
          }
-         
+
          return false;
       }
    }
 
+   /**
+    * Implements the {@link #finish()} method to maintain the state contract of the interface
+    * and delegates event handling to the {@link #onFinish(Object)} method. Provides a fault
+    * barrier to block exceptions in third party code
+    *
+    * @throws IllegalStateException if the update process has already been completed.
+    */
    @Override
    public final void finish(R result)
    {
-      try 
+      try
       {
          synchronized (state)
          {
@@ -53,16 +75,23 @@ public class DataUpdateObserverAdapter<R> implements DataUpdateObserver<R>
             state = State.COMPLETED;
          }
       }
-      catch (Exception e) 
+      catch (Exception e)
       {
          logger.log(Level.SEVERE, "DataUpdateAdapter error: onFinish failed [" + this + "]", e);
       }
    }
 
+   /**
+    * Implements the {@link #aborted()} method to maintain the state contract of the interface
+    * and delegates event handling to the {@link #onAborted()} method. Provides a fault
+    * barrier to block exceptions in third party code
+    *
+    * @throws IllegalStateException if the update process has already been completed.
+    */
    @Override
    public void aborted()
    {
-      try 
+      try
       {
          synchronized (state)
          {
@@ -73,13 +102,20 @@ public class DataUpdateObserverAdapter<R> implements DataUpdateObserver<R>
             state = State.ABORTED;
          }
       }
-      catch (Exception e) 
+      catch (Exception e)
       {
          logger.log(Level.SEVERE, "DataUpdateAdapter error: onAborted failed [" + this + "]", e);
       }
-      
+
    }
 
+   /**
+    * Implements the {@link #error(String, Exception)} method to maintain the state contract
+    * of the interface and delegates event handling to the {@link #onError(String, Exception)}
+    * method. Provides a fault barrier to block exceptions in third party code
+    *
+    * @throws IllegalStateException if the update process has already been completed.
+    */
    @Override
    public final void error(String message, Exception ex)
    {
@@ -87,14 +123,14 @@ public class DataUpdateObserverAdapter<R> implements DataUpdateObserver<R>
       {
          if (isCompleted())
             throw new IllegalStateException();
-         
+
          this.state = State.ERROR;
 
-         try 
+         try
          {
             this.onError(message, ex);
          }
-         catch (Exception e) 
+         catch (Exception e)
          {
             e.addSuppressed(ex);
             logger.log(Level.SEVERE, "DataUpdateAdapter error: onError failed. '" + message + "' [" + ex + "]", e);
@@ -107,7 +143,7 @@ public class DataUpdateObserverAdapter<R> implements DataUpdateObserver<R>
    {
       return cancelled;
    }
-   
+
    public final void cancel()
    {
       this.cancelled = true;
@@ -128,11 +164,11 @@ public class DataUpdateObserverAdapter<R> implements DataUpdateObserver<R>
    protected void onAborted()
    {
       // default no-op implementation
-      
+
    }
 
    /**
-    * Allows 
+    * Allows
     * @return {@code false} If processing should be aborted at this stage.
     */
    protected boolean onStart() {
