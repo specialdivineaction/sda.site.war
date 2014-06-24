bb7d221598a6ca14f2fd117b36849bf7b7441e9c
Neal Audenaert <nealaudenaert@tees.tamu.edu>
2014-06-24 12:37:46 -0500

    Added minimal javadoc; removed dependency on implementation
    https://issues.citd.tamu.edu/browse/CE-4

--- bundles/edu.tamu.tcat.sda.catalog.rest/edu.tamu.tcat.sda.catalog.rest/src/edu/tamu/tcat/sda/catalog/rest/ErrorResponseData.java
+++ bundles/edu.tamu.tcat.sda.catalog.rest/edu.tamu.tcat.sda.catalog.rest/src/edu/tamu/tcat/sda/catalog/rest/ErrorResponseData.java
@@ -8,22 +8,25 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import edu.tamu.tcat.oss.osgi.config.ConfigurationProperties;
-import edu.tamu.tcat.sda.catalog.rest.PeopleResource.CreatePersonERD;
 
-public class ErrorResponseData<T> 
+/**
+ *
+ * @param <T> Must be JSON serializable.
+ */
+public class ErrorResponseData<T>
 {
    public static final String PROP_ENABLE_ERR_DETAILS = "rest.err.details.enabled";
-   
+
    public T resource;
    public Response.Status status;
    public String message;
    public String details;
-   
+
    public ErrorResponseData()
    {
-      
+
    }
-   
+
    ErrorResponseData(T resource, Response.Status status, String message, String detail)
    {
       this.resource = resource;
@@ -31,8 +34,13 @@ public class ErrorResponseData<T>
       this.message = message;
       this.details = detail != null ? detail : message;
    }
-   
-   public static Response createJsonResponse(CreatePersonERD error)
+
+   /**
+    *
+    * @param error
+    * @return
+    */
+   public static <X> Response createJsonResponse(ErrorResponseData<X> error)
    {
       Response resp = Response.status(error.status)
                               .entity(error)
@@ -40,14 +48,14 @@ public class ErrorResponseData<T>
                               .build();
       return resp;
    }
-   
+
    public static String getErrorDetail(Exception ex, ConfigurationProperties properties)
    {
       Boolean enableDetails = properties.getPropertyValue(PROP_ENABLE_ERR_DETAILS, Boolean.class, Boolean.valueOf(false));
-      
+
       if (enableDetails.booleanValue())
          return null;
-      
+
       try (StringWriter sw = new StringWriter();
            PrintWriter writer = new PrintWriter(sw))
       {
