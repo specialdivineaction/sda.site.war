0a9d066c2d8eaff53994f03b22d27d6bbc46d4d3
Neal Audenaert <nealaudenaert@tees.tamu.edu>
2014-06-24 14:44:08 -0500

    Updated AuthorRef and Work constructors; removed Person ref from author ref
    https://issues.citd.tamu.edu/browse/CE-4

--- bundles/edu.tamu.tcat.sda.catalog/edu.tamu.tcat.sda.catalog/src/edu/tamu/tcat/sda/catalog/works/AuthorReference.java
+++ bundles/edu.tamu.tcat.sda.catalog/edu.tamu.tcat.sda.catalog/src/edu/tamu/tcat/sda/catalog/works/AuthorReference.java
@@ -3,31 +3,32 @@ package edu.tamu.tcat.sda.catalog.works;
 import edu.tamu.tcat.sda.catalog.people.Person;
 
 /**
- * Links the author of a work to a biographical record of the person. This allows the 
+ * Links the author of a work to a biographical record of the person. This allows the
  * bibliographic record to capture the name of the author as it appears on the work, along with
  * the role the author played in the creation of this work (e.g., author, translator, editor,
  * director, etc).
+ *
+ * <p>This can be understood as a join structure between bibliographic records and people
+ * where the join record has additional information.
+ *
+ * @see WorkRepository#getAuthor(AuthorReference) To retrieve the {@link Person} associated
+ * with an {@code AuthorReference}.
  */
 public interface AuthorReference
 {
    /**
-    * @return Information about the person represented by this author. Will not be {@code null}. 
-    *    For authors for whom no biographical record is available this should return a special
-    *    person instance representing an unknown or unavailable person.
+    * @return The unique identifier of the referenced author.
     */
-   Person getAuthor();     // TODO need to flesh out how to reference unavailable people.
-   
+   String getId();
+
    /**
     * @return The name of the author as it appears on the work.
     */
-   String getName();    // TODO need PersonName structured representation.
-   
+   String getName();
+
    /**
-    * @return The role this person played in the creation of the work, for example, author, 
+    * @return The role this person played in the creation of the work, for example, author,
     *    translator, editor, director, etc.. This is an application specific value.
     */
    String getRole();
-   
-   String getId();
-   
 }
--- bundles/edu.tamu.tcat.sda.catalog/edu.tamu.tcat.sda.catalog/src/edu/tamu/tcat/sda/catalog/works/dv/AuthorRefDV.java
+++ bundles/edu.tamu.tcat.sda.catalog/edu.tamu.tcat.sda.catalog/src/edu/tamu/tcat/sda/catalog/works/dv/AuthorRefDV.java
@@ -1,6 +1,5 @@
 package edu.tamu.tcat.sda.catalog.works.dv;
 
-import edu.tamu.tcat.sda.catalog.people.Person;
 import edu.tamu.tcat.sda.catalog.works.AuthorReference;
 
 public class AuthorRefDV
@@ -8,16 +7,14 @@ public class AuthorRefDV
    public String authorId;
    public String name;
    public String role;
-   public Person histFigure;
-   
+
    public AuthorRefDV(AuthorReference author)
    {
       this.authorId = author.getId();
       this.name = author.getName();
       this.role = author.getRole();
-      this.histFigure = author.getAuthor();
    }
-   
+
    public AuthorRefDV()
    {
    }
