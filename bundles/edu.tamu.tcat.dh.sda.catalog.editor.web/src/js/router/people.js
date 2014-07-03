23d1c1c5fe52fee69349c441f8fe6b548bc746b9
Matthew Barry <mbarry@tamu.edu>
2014-07-03 15:45:15 -0500

    Stub methods for editing people and works
    https://issues.citd.tamu.edu/browse/CE-12

--- bundles/edu.tamu.tcat.dh.sda.catalog.editor.web/src/js/router/people.js
+++ bundles/edu.tamu.tcat.dh.sda.catalog.editor.web/src/js/router/people.js
@@ -4,13 +4,16 @@ define(function (require) {
         $        = require('jquery'),
 
         PersonFormView = require('js/view/person/form'),
-        Person         = require('js/model/person');
+        Person         = require('js/model/person'),
+
+        Message = require('js/view/message');
 
 
     var PeopleRouter = Backbone.Router.extend({
 
         routes: {
-            'people/new': 'newAction'
+            'people/new': 'newAction',
+            'people/:id': 'editAction'
         },
 
         newAction: function () {
@@ -20,6 +23,29 @@ define(function (require) {
             });
 
             $('#page-title').text('Add Person');
+            $('#content').html(form.render().el);
+        },
+
+        editAction: function (id) {
+            var person = new Person({ id: id });
+
+            person.fetch({
+                error: function (model, response, options) {
+                    var msg = new Message({
+                        type: 'error',
+                        message: 'Unable to load person with ID: <code>' + id + '</code>'
+                    });
+
+                    msg.render();
+                }
+            });
+
+            var form = new PersonFormView({
+                model: person,
+                router: this
+            });
+
+            $('#page-title').text('Edit Person');
             $('#content').html(form.render().el);
         }
 
--- bundles/edu.tamu.tcat.dh.sda.catalog.editor.web/src/js/router/works.js
+++ bundles/edu.tamu.tcat.dh.sda.catalog.editor.web/src/js/router/works.js
@@ -4,13 +4,16 @@ define(function (require) {
         $        = require('jquery'),
 
         WorkFormView = require('js/view/work/form'),
-        Work         = require('js/model/work');
+        Work         = require('js/model/work'),
+
+        Message = require('js/view/message');
 
 
     var WorksRouter = Backbone.Router.extend({
 
         routes: {
-            'works/new': 'newAction'
+            'works/new': 'newAction',
+            'works/:id': 'editAction'
         },
 
         newAction: function () {
@@ -20,6 +23,30 @@ define(function (require) {
             });
 
             $('#page-title').text('Add Book');
+            $('#content').html(form.render().el);
+        },
+
+        editAction: function (id) {
+            var work = new Work({ id: id });
+
+            work.fetch({
+                error: function (model, response, error) {
+                    var msg = new Message({
+                        type: 'error',
+                        message: 'Unable to load work with ID: <code>' + id + '</code>'
+                    });
+
+                    msg.render();
+                }
+            });
+
+            // fetch will update the form when it loads
+            var form = new WorkFormView({
+                model: work,
+                router: this
+            });
+
+            $('#page-title').text('Edit Work');
             $('#content').html(form.render().el);
         }
 
