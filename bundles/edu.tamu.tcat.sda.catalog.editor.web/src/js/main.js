935da41de7bf7c4422d538b7a389f7acde49f69b
Matthew Barry <mbarry@tamu.edu>
2014-07-31 17:28:02 -0500

    Implemented search forms in main layout.

--- bundles/edu.tamu.tcat.sda.catalog.editor.web/src/js/main.js
+++ bundles/edu.tamu.tcat.sda.catalog.editor.web/src/js/main.js
@@ -8,7 +8,8 @@ define(function (require) {
         WorksRouter  = require('js/router/works'),
         SearchForm   = require('js/view/search_form'),
 
-        Message      = require('js/view/message');
+        Message      = require('js/view/message'),
+        Config       = require('js/config');
 
     require('bootstrap');
 
@@ -27,25 +28,55 @@ define(function (require) {
         type: 'books',
         search: function (query) {
             return new Promise(function (resolve, reject) {
-                if (query === '') resolve([]);
-
-                // HACK: replace with something a little more ajaxy
-                var results = [];
-                for (var i = 0; i < 10; i++) {
-                    results.push({
-                        content: '<strong>Result ' + i + '</strong><br/>Lorem ipsum dolor sit amet...',
-                    });
+                if (query === '') {
+                    reject(new Error('No query given.'));
+                    return;
                 }
 
-                resolve(results);
+                $.getJSON(Config.apiPrefix + '/works', { title: query }, function (data) {
+
+                    console.log(data);
+
+                    // TODO: convert data to result array
+                    resolve(data.map(function (work) {
+                        return {
+                            html: 'TITLE',
+                            href: '#works/WORK_ID'
+                        };
+                    }));
+                }).fail(function (jqxhr, status, errorMessage) {
+                    reject(new Error(errorMessage));
+                });
             });
         }
     });
     $('#sidebar #books .search-form').html(bookSearchForm.render().el);
 
-    var peopleSearchForm = new SearchForm({ type: 'people' });
-    peopleSearchForm.on('search', function (query) {
-        console.log('search for person ' + query);
+    var peopleSearchForm = new SearchForm({
+        type: 'people',
+        search: function (query) {
+            return new Promise(function (resolve, reject) {
+                if (query === '') {
+                    reject(new Error('No query given.'));
+                    return;
+                }
+
+                $.getJSON(Config.apiPrefix + '/people', { lastName: query }, function (data) {
+
+                    console.log(data);
+
+                    // TODO: convert data to result array
+                    resolve(data.map(function (person) {
+                        return {
+                            html: 'NAME',
+                            href: '#people/PERSON_ID'
+                        };
+                    }));
+                }).fail(function (jqxhr, status, errorMessage) {
+                    reject(new Error(errorMessage));
+                });
+            });
+        }
     });
     $('#sidebar #people .search-form').html(peopleSearchForm.render().el);
 
--- bundles/edu.tamu.tcat.sda.catalog.editor.web/src/js/view/search_form.js
+++ bundles/edu.tamu.tcat.sda.catalog.editor.web/src/js/view/search_form.js
@@ -1,7 +1,9 @@
 define(function (require) {
 
     var Backbone = require('backbone'),
-        _        = require('underscore');
+        _        = require('underscore'),
+
+        Message  = require('js/view/message');
 
 
     var SearchForm = Backbone.View.extend({
@@ -16,7 +18,7 @@ define(function (require) {
              * Results are of the form:
              *
              * {
-             *     content: 'text to display',     // REQUIRED
+             *     html: 'text to display',        // REQUIRED
              *     href: '#path/to/resource',      // (default: '#')
              *     click: function (event) {
              *         // optional click handler
@@ -42,16 +44,32 @@ define(function (require) {
             var $resultContainer = this.$('.search-results').empty();
 
             resultsPromise.then(function (results) {
+                if (results.length === 0) {
+                    message = new Message({
+                        message: 'No results found.',
+                        ttl: 5000
+                    });
+                    message.render().openIn($resultContainer);
+                    return;
+                }
+
                 _.each(results, function (result) {
                     var resultLink = $('<a>', {
                         'class': 'list-group-item',
                         'href': result.href || '#',
-                        'html': result.content
+                        'html': result.html
                     }).appendTo($resultContainer);
 
                     if (_.isFunction(result.click))
                         resultLink.on('click', result, result.click);
                 });
+            }, function (error) {
+                message = new Message({
+                    type: 'error',
+                    message: 'Unable to fetch results: ' + error.message,
+                    ttl: 5000
+                });
+                message.render().openIn($resultContainer);
             });
         },
 
