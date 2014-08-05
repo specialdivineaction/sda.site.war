0b40145dc4a65ccbc32d7e9bf639281c2b1f3544
Jesse Mitchell <jessemitchell@tees.tamus.edu>
2014-08-05 16:36:38 -0500

    Added functionality to update People using PUT request

--- tests/edu.tamu.tcat.sda.catalog.psql.test/src/edu/tamu/tcat/sda/catalog/psql/test/restapi/TestPeopleRESTapi.java
+++ tests/edu.tamu.tcat.sda.catalog.psql.test/src/edu/tamu/tcat/sda/catalog/psql/test/restapi/TestPeopleRESTapi.java
@@ -20,6 +20,7 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -46,6 +47,7 @@ public class TestPeopleRESTapi
 {
    private static HttpGet  get;
    private static HttpPost post;
+   private static HttpPut put;
    private static HttpDelete delete;
    private static CloseableHttpClient client;
    private static URI uri;
@@ -63,6 +65,10 @@ public class TestPeopleRESTapi
       post = new HttpPost(uri);
       post.setHeader("User-Agent", "Mozilla/5.0");
       post.setHeader("Content-type", "application/json");
+
+      put = new HttpPut(uri);
+      put.setHeader("User-Agent", "Mozilla/5.0");
+      put.setHeader("Content-type", "application/json");
 
       get = new HttpGet(uri);
       get.setHeader("User-Agent", "Mozilla/5.0");
@@ -105,14 +111,46 @@ public class TestPeopleRESTapi
    }
 
    @Test
+   public void testPut() throws Exception
+   {
+      post.setEntity(new StringEntity(providPerson()));
+
+      HttpResponse response = client.execute(post);
+      PersonDV createdPerson = null;
+      try(InputStream content = response.getEntity().getContent())
+      {
+         createdPerson = mapper.parse(content, PersonDV.class);
+      }
+      catch (IOException e)
+      {
+         throw new IOException("Could not retrieve response.");
+      }
+
+      createdPerson.summary = "The old summary was just not working";
+
+      URI personUri = uri.resolve("people/" + createdPerson.id);
+      put.setURI(personUri);
+      String updatedJson = mapper.asString(createdPerson);
+      put.setEntity(new StringEntity(updatedJson));
+      HttpResponse putResponse = client.execute(put);
+      PersonDV updatedPerson = null;
+      try(InputStream updatedContent = putResponse.getEntity().getContent())
+      {
+         updatedPerson = mapper.parse(updatedContent, PersonDV.class);
+      }
+      catch (IOException e)
+      {
+         throw new IOException("Could not retrieve response.");
+      }
+
+      Assert.assertEquals(createdPerson.summary, updatedPerson.summary);
+
+   }
+
+   @Test
    public void testPost() throws Exception
    {
-
-      People person = new People();
-      PersonDV buildPerson = person.buildPerson();
-      String json = mapper.asString(buildPerson);
-
-      post.setEntity(new StringEntity(json));
+      post.setEntity(new StringEntity(providPerson()));
 
       HttpResponse response = client.execute(post);
 
@@ -126,9 +164,19 @@ public class TestPeopleRESTapi
       else
          Assert.fail("Server Error: " + statusCode);
 
-      InputStream content = response.getEntity().getContent();
-//      PersonDV person = mapper.fromJSON(content, type)
+      try (InputStream content = response.getEntity().getContent())
+      {
+         PersonDV createdPerson = mapper.parse(content, PersonDV.class);
 
+         Assert.assertEquals(createdPerson.names, createdPerson.names);
+         Assert.assertEquals(createdPerson.birth, createdPerson.birth);
+         Assert.assertEquals(createdPerson.death, createdPerson.death);
+         Assert.assertEquals(createdPerson.summary, createdPerson.summary);
+      }
+      catch(IOException e)
+      {
+         throw new IOException("Could not retrieve the post response");
+      }
    }
 
    @Test
@@ -168,15 +216,28 @@ public class TestPeopleRESTapi
    }
 
    @Test
-   public void testGetPerson()
+   public void testGetPerson() throws Exception
    {
+      post.setEntity(new StringEntity(providPerson()));
+
+      HttpResponse response = client.execute(post);
+      PersonDV newPerson = null;
+      try(InputStream updatedContent = response.getEntity().getContent())
+      {
+         newPerson = mapper.parse(updatedContent, PersonDV.class);
+      }
+      catch (IOException e)
+      {
+         throw new IOException("Could not retrieve response.");
+      }
       try
       {
-         URI personUri = uri.resolve("people/1");
+         URI personUri = uri.resolve("people/" + newPerson.id);
          get.setURI(personUri);
-         CloseableHttpResponse response = client.execute(get);
-         InputStream content = response.getEntity().getContent();
-         StatusLine statusLine = response.getStatusLine();
+
+         CloseableHttpResponse clResponse = client.execute(get);
+         InputStream content = clResponse.getEntity().getContent();
+         StatusLine statusLine = clResponse.getStatusLine();
 
          if (statusLine.getStatusCode() < 300)
          {
@@ -204,6 +265,13 @@ public class TestPeopleRESTapi
       {
          Assert.fail("IOException" + e);
       }
+   }
+
+   private String providPerson() throws JsonException
+   {
+      People person = new People();
+      PersonDV buildPerson = person.buildPerson();
+      return mapper.asString(buildPerson);
    }
 
 
