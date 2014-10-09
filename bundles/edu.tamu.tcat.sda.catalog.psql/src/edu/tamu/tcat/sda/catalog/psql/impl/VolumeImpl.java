79e5dd16667404795467a12597d68480c3814c03
Matthew Barry <mbarry@tamu.edu>
2014-10-09 16:03:38 -0500

    Implementation classes for Edition and Volume.
    https://issues.citd.tamu.edu/browse/CE-43

---
+++ bundles/edu.tamu.tcat.sda.catalog.psql/src/edu/tamu/tcat/sda/catalog/psql/impl/VolumeImpl.java
@@ -0,0 +1,100 @@
+package edu.tamu.tcat.sda.catalog.psql.impl;
+
+import java.net.URI;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import edu.tamu.tcat.sda.catalog.works.AuthorReference;
+import edu.tamu.tcat.sda.catalog.works.Title;
+import edu.tamu.tcat.sda.catalog.works.Volume;
+import edu.tamu.tcat.sda.catalog.works.dv.VolumeDV;
+
+public class VolumeImpl implements Volume
+{
+   private String volume;
+   private List<AuthorReference> authors;
+   private List<Title> titles;
+   private String summary;
+   private String series;
+   private List<URI> images;
+   private Collection<String> tags;
+   private Collection<String> notes;
+
+
+   public VolumeImpl()
+   {
+   }
+
+   public VolumeImpl(VolumeDV dv)
+   {
+      volume = dv.volume;
+
+      authors = dv.authors.stream()
+            .map((a) -> new AuthorReferenceImpl(a))
+            .collect(Collectors.toList());
+
+      titles = dv.titles.stream()
+            .map((t) -> new TitleImpl(t))
+            .collect(Collectors.toList());
+
+      summary = dv.summary;
+
+      series = dv.series;
+
+      images = dv.images;
+
+      tags = dv.tags;
+
+      notes = dv.notes;
+   }
+
+   @Override
+   public String getVolume()
+   {
+      return volume;
+   }
+
+   @Override
+   public List<AuthorReference> getAuthors()
+   {
+      return authors;
+   }
+
+   @Override
+   public List<Title> getTitles()
+   {
+      return titles;
+   }
+
+   @Override
+   public String getSummary()
+   {
+      return summary;
+   }
+
+   @Override
+   public String getSeries()
+   {
+      return series;
+   }
+
+   @Override
+   public List<URI> getImages()
+   {
+      return images;
+   }
+
+   @Override
+   public Collection<String> getTags()
+   {
+      return tags;
+   }
+
+   @Override
+   public Collection<String> getNotes()
+   {
+      return notes;
+   }
+
+}
