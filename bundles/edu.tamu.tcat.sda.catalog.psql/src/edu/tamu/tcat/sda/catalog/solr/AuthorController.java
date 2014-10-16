552c631b5a5c3946dcd690875cf07dfa8630074d
Neal Audenaert <nealaudenaert@tees.tamu.edu>
2014-10-16 12:59:21 -0500

    Revised data model for author docs in solr
    https://issues.citd.tamu.edu/browse/SDA-2

--- bundles/edu.tamu.tcat.sda.catalog.psql/src/edu/tamu/tcat/sda/catalog/solr/AuthorController.java
+++ bundles/edu.tamu.tcat.sda.catalog.psql/src/edu/tamu/tcat/sda/catalog/solr/AuthorController.java
@@ -1,9 +1,9 @@
 package edu.tamu.tcat.sda.catalog.solr;
 
 import java.io.IOException;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.logging.Logger;
 
@@ -18,71 +18,128 @@ import edu.tamu.tcat.sda.catalog.people.dv.PersonNameDV;
 
 public class AuthorController
 {
-
    Logger log = Logger.getLogger("edu.tamu.tcat.sda.catalog.solr.authorcontroller");
-   private final String solrBaseUri = "https://sda-dev.citd.tamu.edu/solr/";
    private final SolrServer solr;
 
-   private final static String authorId = "id";
-   private final static String authorTitle = "title";
-   private final static String givenName = "givenName";
-   private final static String middleName = "middleName";
+   private final static String personId = "id";
    private final static String familyName = "familyName";
-   private final static String suffix = "suffix";
+   private final static String syntheticName = "syntheticName";
    private final static String displayName = "displayName";
-   private final static String birthTitle = "bTitle";
-   private final static String birthDescription = "bDescript";
    private final static String birthLocation = "bLocation";
    private final static String birthDate = "bDate";
-   private final static String deathTitle = "dTitle";
-   private final static String deathDescription = "dDescript";
    private final static String deathLocation = "dLocation";
    private final static String deathDate = "dDate";
    private final static String summary = "summary";
 
+   private String collectionName = "authors";
+   private String rootSolrEndpoint = "https://sda-dev.citd.tamu.edu/solr/";
+
    public AuthorController()
    {
-      solr = new HttpSolrServer(solrBaseUri + "authors");
+      solr = new HttpSolrServer(rootSolrEndpoint + collectionName);
    }
 
-   public void addDocument(PersonDV author)
+   private String guardNull(String value)
+   {
+      return value == null ? "" : value;
+   }
+
+   private String convertDate(Date event)
+   {
+      // TODO we need some more tooling around dates.
+      String dateRep = "";
+
+      SimpleDateFormat calendar = new SimpleDateFormat("yyyy-MM-dd");
+      SimpleDateFormat time = new SimpleDateFormat("HH:mm:SS");
+      dateRep = calendar.format(event) + "T" + time.format(event) + "Z";
+
+      return dateRep;
+   }
+
+//   <T> SolrDocumentAdapter<T> getAdapter(Class<T> type) throws UnsupportedTypeException
+//   {
+//      // lookup from some registry
+//
+//      return null;
+//   }
+
+   /**
+    * Adapts the supplied data vehicle into a {@link SolrInputDocument}.
+    *
+    * @param person The data to bee added to the database.
+    * @return The document to index.
+    */
+   private SolrInputDocument adapt(PersonDV person)
    {
       SolrInputDocument document = new SolrInputDocument();
 
-
-      document.addField(authorId, author.id);
-
-      Set<PersonNameDV> authors = author.names;
-      for(PersonNameDV authorDV : authors)
+      document.addField(personId, person.id);
+      document.addField(syntheticName, constructSyntheticName(person.names));
+      for(PersonNameDV name : person.names)
       {
-         document.addField(authorTitle, authorDV.title == null ? "" : authorDV.title);
-         document.addField(givenName, authorDV.givenName == null ? "" : authorDV.givenName);
-         document.addField(middleName, authorDV.middleName == null ? "" : authorDV.middleName);
-         document.addField(familyName, authorDV.familyName == null ? "" : authorDV.familyName);
-         document.addField(suffix, authorDV.suffix == null ? "" : authorDV.suffix);
-         document.addField(displayName,  authorDV.displayName == null ? "" : authorDV.displayName);
+         document.addField(familyName, guardNull(name.familyName));
+         document.addField(displayName, guardNull(name.displayName));
       }
 
-      HistoricalEventDV birth = author.birth;
-      document.addField(birthTitle, birth.title == null ? "" : birth.title);
-      document.addField(birthDescription, birth.description == null ? "" : birth.description);
+      HistoricalEventDV birth = person.birth;
       document.addField(birthLocation, birth.location == null ? "" : birth.location);
       Date bDate = birth.eventDate;
       if (bDate != null)
-         document.addField(birthDate, convertDate(birth.eventDate));
+         document.addField(birthDate, convertDate(bDate));
 
-      HistoricalEventDV death = author.birth;
-      document.addField(deathTitle, death.title == null ? "" : death.title);
-      document.addField(deathDescription, death.description == null ? "" : death.description);
+      HistoricalEventDV death = person.birth;
       document.addField(deathLocation, death.location == null ? "" : death.location);
       if (death.eventDate != null)
          document.addField(deathDate, convertDate(death.eventDate));
 
-      document.addField(summary, author.summary == null ? "" : author.summary);
+      document.addField(summary, guardNull(person.summary));
+
+      // TODO need default display name
+      //      maybe first sentence of summary
+      // 'defualtDisplayName' : Neal Audenaert (1978 - )
+      // First sentence of summary
+      return document;
+   }
+
+   /**
+    * Constructs a synthetic name that contains the various values (title, first name,
+    * family name, etc) from different names associated with this person. Each portion
+    * of a person's name is collected into a set of 'name parts' that is then concatenated
+    * to form a string-valued synthetic name. This allows all of the various name tokens to
+    * be included in the search.
+    *
+    * @param names A set of names associated with a person.
+    * @return A synthetic name that contains a union of the different name fields.
+    */
+   private String constructSyntheticName(Set<PersonNameDV> names)
+   {
+      Set<String> nameParts = new HashSet<>();
+      for(PersonNameDV name : names)
+      {
+         nameParts.add(name.title);
+         nameParts.add(name.givenName);
+         nameParts.add(name.middleName);
+         nameParts.add(name.familyName);
+      }
+
+      StringBuilder sb = new StringBuilder();
+      for (String part : nameParts)
+      {
+         if (part == null)
+            continue;
+
+         sb.append(part).append(" ");
+      }
+
+      return sb.toString().trim();
+   }
+
+   public void addDocument(PersonDV author)
+   {
+      SolrInputDocument document = adapt(author);
 
       try
       {
-
          solr.add(document);
          solr.commit();
       }
@@ -115,16 +172,5 @@ public class AuthorController
    public void reindex()
    {
 
-   }
-
-   private String convertDate(Date event)
-   {
-      String dateRep = "";
-
-      SimpleDateFormat calendar = new SimpleDateFormat("yyyy-MM-dd");
-      SimpleDateFormat time = new SimpleDateFormat("HH:mm:SS");
-      dateRep = calendar.format(event) + "T" + time.format(event) + "Z";
-
-      return dateRep;
    }
 }
