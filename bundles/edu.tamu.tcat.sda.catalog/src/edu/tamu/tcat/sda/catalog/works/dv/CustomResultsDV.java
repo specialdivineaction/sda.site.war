51a6593ada2b15c8884015f21a5e7ed320f1cd55
Jesse Mitchell <jessemitchell@tees.tamus.edu>
2014-12-05 16:57:58 -0600

    Added CustomResultsDV to return work/edition/volume id https://issues.citd.tamu.edu/browse/CE-18

---
+++ bundles/edu.tamu.tcat.sda.catalog/src/edu/tamu/tcat/sda/catalog/works/dv/CustomResultsDV.java
@@ -0,0 +1,14 @@
+package edu.tamu.tcat.sda.catalog.works.dv;
+
+public class CustomResultsDV
+{
+   public String id;
+
+   public CustomResultsDV(String id)
+   {
+      this.id = id;
+   }
+   public CustomResultsDV()
+   {
+   }
+}
