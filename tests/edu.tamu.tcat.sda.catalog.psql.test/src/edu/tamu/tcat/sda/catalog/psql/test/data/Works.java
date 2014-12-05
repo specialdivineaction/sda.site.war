a56ce8b050090247b458dc1e49dc6cf830c11273
Jesse Mitchell <jessemitchell@tees.tamus.edu>
2014-12-05 17:06:39 -0600

    Changed JUnit Test to work with new Rest API implementations https://issues.citd.tamu.edu/browse/CE-18

--- tests/edu.tamu.tcat.sda.catalog.psql.test/src/edu/tamu/tcat/sda/catalog/psql/test/data/Works.java
+++ tests/edu.tamu.tcat.sda.catalog.psql.test/src/edu/tamu/tcat/sda/catalog/psql/test/data/Works.java
@@ -1,6 +1,8 @@
 package edu.tamu.tcat.sda.catalog.psql.test.data;
 
+import java.net.URI;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.GregorianCalendar;
 import java.util.HashSet;
 import java.util.List;
@@ -8,43 +10,96 @@ import java.util.Set;
 
 import edu.tamu.tcat.sda.catalog.works.dv.AuthorRefDV;
 import edu.tamu.tcat.sda.catalog.works.dv.DateDescriptionDV;
+import edu.tamu.tcat.sda.catalog.works.dv.EditionDV;
 import edu.tamu.tcat.sda.catalog.works.dv.PublicationInfoDV;
 import edu.tamu.tcat.sda.catalog.works.dv.TitleDV;
+import edu.tamu.tcat.sda.catalog.works.dv.VolumeDV;
 import edu.tamu.tcat.sda.catalog.works.dv.WorkDV;
 
 public class Works
 {
-   public WorkDV workDV;
-   private AuthorRefDV authorRef;
-   private Set<TitleDV> titleSet;
-   private DateDescriptionDV dateDescript;
-   private PublicationInfoDV pubInfo;
-
-   List<AuthorRefDV> authorList = new ArrayList<>();
-   List<AuthorRefDV> otherAuthorList = new ArrayList<>();
-
    public Works()
    {
-      workDV = new WorkDV();
-      authorRef = new AuthorRefDV();
-      titleSet = new HashSet<TitleDV>();
-      dateDescript = new DateDescriptionDV();
-      pubInfo = new PublicationInfoDV();
    }
 
-   public WorkDV buildWork()
+   public WorkDV addWork()
    {
-      return build();
+      WorkDV workDV = new WorkDV();
+      workDV.authors = addAuthors();
+      workDV.otherAuthors = addAuthors();
+      workDV.titles = addTitles();
+      workDV.pubInfo = addPublication();
+      workDV.series = "Series 1";
+      workDV.summary = "Summary of the work";
+      return workDV;
    }
 
-   private WorkDV build()
+   public EditionDV addEdition()
    {
-       authorRef.authorId = "1234";
-       authorRef.name = "A.C. Dixon";
-       authorRef.role = "Autor";
-       authorList.add(authorRef);
+      EditionDV edition = new EditionDV();
+      edition.editionName = "";
+      edition.authors = addAuthors();
+      edition.otherAuthors = addAuthors();
+      edition.titles = addTitles();
+      edition.publicationInfo = addPublication();
+      edition.series = "";
+      edition.summary = "";
+      return edition;
+   }
 
+   public VolumeDV addVolume()
+   {
+      VolumeDV volume = new VolumeDV();
+      volume.volumeNumber = "I";
+      volume.authors = addAuthors();
+      volume.otherAuthors = addAuthors();
+      volume.titles = addTitles();
+      volume.images = addImages();
+      volume.tags = addTags();
+      volume.notes = addNotes();
+      volume.series = "Volume Series 1";
+      volume.summary = "Volume summary";
+      return volume;
+   }
 
+   Collection<String> addNotes()
+   {
+      Set<String> notes = new HashSet<>();
+      notes.add("note one");
+      notes.add("note two");
+      notes.add("note three");
+      notes.add("note four");
+      return notes;
+   }
+
+   Collection<String> addTags()
+   {
+      Set<String> tags = new HashSet<>();
+      tags.add("Tag one");
+      tags.add("Tag two");
+      return tags;
+   }
+
+   List<URI> addImages()
+   {
+      List<URI> images = new ArrayList<>();
+      return images;
+   }
+
+   List<AuthorRefDV> addAuthors()
+   {
+      List<AuthorRefDV> authorList = new ArrayList<>();
+      AuthorRefDV authorRef = new AuthorRefDV();
+      authorRef.authorId = "1234";
+      authorRef.name = "A.C. Dixon";
+      authorRef.role = "Autor";
+      authorList.add(authorRef);
+      return authorList;
+   }
+
+   Collection<TitleDV> addTitles()
+   {
+      Set<TitleDV> titleSet = new HashSet<TitleDV>();
       TitleDV canonical = new TitleDV();
       canonical.title = "Canonical Full Title Name";
       canonical.subtitle = "With subtitle";
@@ -67,23 +122,22 @@ public class Works
       titleSet.add(canonical);
       titleSet.add(shortTitle);
       titleSet.add(localeTitle);
+      return titleSet;
+   }
 
-      dateDescript = new DateDescriptionDV();
+   PublicationInfoDV addPublication()
+   {
+      PublicationInfoDV pubInfo = new PublicationInfoDV();
+
+      DateDescriptionDV dateDescript = new DateDescriptionDV();
       dateDescript.display = "";
       dateDescript.value = new GregorianCalendar(1856, 1, 28).getTime();
 
-      pubInfo = new PublicationInfoDV();
       pubInfo.date = dateDescript;
-      pubInfo.place = "";
-      pubInfo.publisher = "";
+      pubInfo.place = "London";
+      pubInfo.publisher = "J.C. Publishing";
 
-      workDV.authors = authorList;
-      workDV.otherAuthors = otherAuthorList;
-      workDV.titles = titleSet;
-      workDV.pubInfo = pubInfo;
-      workDV.series = "Series 1";
-      workDV.summary = "Summary of the work";
-
-      return workDV;
+      return pubInfo;
    }
+
 }
