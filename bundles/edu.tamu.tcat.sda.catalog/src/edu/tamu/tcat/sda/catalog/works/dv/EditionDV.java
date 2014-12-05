e09ef40dcd5a4eec1910a62eb0cd5c50744a6783
Jesse Mitchell <jessemitchell@tees.tamus.edu>
2014-12-05 17:01:47 -0600

    Initialized EditionDV to have an empty list of volumes https://issues.citd.tamu.edu/browse/CE-18

--- bundles/edu.tamu.tcat.sda.catalog/src/edu/tamu/tcat/sda/catalog/works/dv/EditionDV.java
+++ bundles/edu.tamu.tcat.sda.catalog/src/edu/tamu/tcat/sda/catalog/works/dv/EditionDV.java
@@ -1,5 +1,6 @@
 package edu.tamu.tcat.sda.catalog.works.dv;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -11,12 +12,13 @@ public class EditionDV
    public String id;
    public String editionName;
    public PublicationInfoDV publicationInfo;
-   public List<VolumeDV> volumes;
    public List<AuthorRefDV> authors;
    public Collection<TitleDV> titles;
    public List<AuthorRefDV> otherAuthors;
    public String summary;
    public String series;
+   // Hack: Editions may not contain volumes by default and can be added later.
+   public List<VolumeDV> volumes = new ArrayList<VolumeDV>();
 
    public EditionDV(Edition ed)
    {
