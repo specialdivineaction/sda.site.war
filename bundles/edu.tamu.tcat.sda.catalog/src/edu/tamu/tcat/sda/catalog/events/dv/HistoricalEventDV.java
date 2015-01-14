1fed950d9ba744928c5963c1aa698cb936603e96
Neal Audenaert <nealaudenaert@tees.tamu.edu>
2015-01-14 17:00:07 -0600

    Updated DVs to reflect API changes
    https://issues.citd.tamu.edu/browse/CE-68

--- bundles/edu.tamu.tcat.sda.catalog/src/edu/tamu/tcat/sda/catalog/events/dv/HistoricalEventDV.java
+++ bundles/edu.tamu.tcat.sda.catalog/src/edu/tamu/tcat/sda/catalog/events/dv/HistoricalEventDV.java
@@ -1,28 +1,29 @@
 package edu.tamu.tcat.sda.catalog.events.dv;
 
-import java.util.Date;
-
 import edu.tamu.tcat.sda.catalog.events.HistoricalEvent;
 
 public class HistoricalEventDV
 {
+
    public String id;
    public String title;
    public String description;
    public String location;
-   public Date eventDate;
-   
+
+   /** The date this event took place. */
+   public DateDescriptionDV eventDate;
+
    public HistoricalEventDV()
    {
-      
+
    }
-   
+
    public HistoricalEventDV(HistoricalEvent orig)
    {
       this.id = orig.getId();
       this.title = orig.getTitle();
       this.description = orig.getDescription();
       this.location = orig.getLocation();
-      this.eventDate = orig.getDate();
+      this.eventDate = new DateDescriptionDV(orig.getDate());
    }
 }
