cf4ece5c959a3944705c20b2e44209af95a68d1b
Jesse Mitchell <jessemitchell@tees.tamus.edu>
2015-01-16 09:54:30 -0600

    Added DB repo with added tasks.

--- bundles/edu.tamu.tcat.sda.catalog.psql/src/edu/tamu/tcat/sda/catalog/relationships/psql/PsqlRelationshipRepo.java
+++ bundles/edu.tamu.tcat.sda.catalog.psql/src/edu/tamu/tcat/sda/catalog/relationships/psql/PsqlRelationshipRepo.java
@@ -1,15 +1,20 @@
 package edu.tamu.tcat.sda.catalog.relationships.psql;
 
 import java.util.Objects;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 import edu.tamu.tcat.db.exec.sql.SqlExecutor;
 import edu.tamu.tcat.oss.json.JsonMapper;
 import edu.tamu.tcat.sda.catalog.IdFactory;
+import edu.tamu.tcat.sda.catalog.psql.impl.EditRelationshipCommandImpl;
 import edu.tamu.tcat.sda.catalog.relationship.EditRelationshipCommand;
 import edu.tamu.tcat.sda.catalog.relationship.Relationship;
 import edu.tamu.tcat.sda.catalog.relationship.RelationshipNotAvailableException;
 import edu.tamu.tcat.sda.catalog.relationship.RelationshipPersistenceException;
 import edu.tamu.tcat.sda.catalog.relationship.RelationshipRepository;
+import edu.tamu.tcat.sda.catalog.relationship.RelationshipTypeRegistry;
+import edu.tamu.tcat.sda.catalog.relationship.model.RelationshipDV;
 
 public class PsqlRelationshipRepo implements RelationshipRepository
 {
@@ -23,6 +28,7 @@ public class PsqlRelationshipRepo implements RelationshipRepository
    private SqlExecutor exec;
    private IdFactory idFactory;
    private JsonMapper jsonMapper;
+   private RelationshipTypeRegistry typeReg;
 
    public void setDatabaseExecutor(SqlExecutor exec)
    {
@@ -37,6 +43,11 @@ public class PsqlRelationshipRepo implements RelationshipRepository
    public void setIdFactory(IdFactory factory)
    {
       this.idFactory = factory;
+   }
+
+   public void setTypeRegistry(RelationshipTypeRegistry typeReg)
+   {
+      this.typeReg = typeReg;
    }
 
    public void activate()
@@ -56,29 +67,57 @@ public class PsqlRelationshipRepo implements RelationshipRepository
    @Override
    public Relationship get(String id) throws RelationshipNotAvailableException, RelationshipPersistenceException
    {
-      // TODO Auto-generated method stub
-      return null;
+      PsqlGetRelationshipTask task = new PsqlGetRelationshipTask(id, jsonMapper, typeReg);
+      try
+      {
+         return exec.submit(task).get();
+      }
+      catch (ExecutionException e)
+      {
+         Throwable cause = e.getCause();
+         if (cause instanceof RelationshipNotAvailableException)
+            throw (RelationshipNotAvailableException)cause;
+         if (cause instanceof RuntimeException)
+            throw (RuntimeException)cause;
+
+         throw new IllegalStateException("Unexpected problems while attempting to retrieve relationship entry [" + id +"]" , e);
+      }
+      catch (InterruptedException e) {
+         throw new IllegalStateException("Failed to retrieve relationship entry [" + id +"]", e);
+      }
    }
 
    @Override
    public EditRelationshipCommand create() throws RelationshipPersistenceException
    {
-      // TODO Auto-generated method stub
-      return null;
+      RelationshipDV relationship = new RelationshipDV();
+      relationship.id = idFactory.getNextId(ID_CONTEXT);
+
+      EditRelationshipCommandImpl command = new EditRelationshipCommandImpl(relationship, idFactory);
+      command.setCommitHook((r) -> {
+         PsqlCreateRelationshipTask task = new PsqlCreateRelationshipTask(r, jsonMapper);
+         Future<String> submitRelationship = exec.submit(task);
+         return submitRelationship;
+      });
+      return command;
    }
 
    @Override
    public EditRelationshipCommand edit(String id) throws RelationshipNotAvailableException, RelationshipPersistenceException
    {
-      // TODO Auto-generated method stub
-      return null;
+      EditRelationshipCommandImpl command = new EditRelationshipCommandImpl(RelationshipDV.create(get(id)) , idFactory);
+      command.setCommitHook((r) -> {
+         PsqlUpdateRelationshipTask task = new PsqlUpdateRelationshipTask(r, jsonMapper);
+         Future<String> submitRelationship = exec.submit(task);
+         return submitRelationship;
+      });
+      return command;
    }
 
    @Override
    public void delete(String id) throws RelationshipNotAvailableException, RelationshipPersistenceException
    {
-      // TODO Auto-generated method stub
-
+      throw new UnsupportedOperationException("not impl");
    }
 
 }
