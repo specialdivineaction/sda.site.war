9769873168140ce8a86c56f7bf4086e2e01747fe
Neal Audenaert <nealaudenaert@tees.tamu.edu>
2015-01-22 17:09:50 -0600

    Created bundle for REST API Impl
    https://issues.citd.tamu.edu/browse/RI-7

---
+++ bundles/edu.tamu.tcat.sda.catalog.relationship.rest/src/edu/tamu/tcat/sda/catalog/relationship/rest/v1/RelationshipService.java
@@ -0,0 +1,11 @@
+package edu.tamu.tcat.sda.catalog.relationship.rest.v1;
+
+import javax.ws.rs.Path;
+
+@Path("/relationships/{id}")
+public class RelationshipService
+{
+
+
+
+}
---
+++ bundles/edu.tamu.tcat.sda.catalog.relationship.rest/src/edu/tamu/tcat/sda/catalog/relationship/rest/v1/RelationshipsCollectionService.java
@@ -0,0 +1,9 @@
+package edu.tamu.tcat.sda.catalog.relationship.rest.v1;
+
+import javax.ws.rs.Path;
+
+@Path("/relationships")
+public class RelationshipsCollectionService
+{
+
+}
