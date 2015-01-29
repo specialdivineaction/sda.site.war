ff450a287855139a5bb650dd2d795d5d73017055
Neal Audenaert <nealaudenaert@tees.tamu.edu>
2015-01-29 09:45:56 -0600

    Added explicit handling of exceptions within REST API.

--- bundles/edu.tamu.tcat.sda.catalog.rest/src/edu/tamu/tcat/sda/catalog/rest/WorksResource.java
+++ bundles/edu.tamu.tcat.sda.catalog.rest/src/edu/tamu/tcat/sda/catalog/rest/WorksResource.java
@@ -5,9 +5,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.InternalServerErrorException;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -20,6 +25,7 @@ import javax.ws.rs.core.UriInfo;
 
 import edu.tamu.tcat.osgi.config.ConfigurationProperties;
 import edu.tamu.tcat.oss.json.JsonException;
+import edu.tamu.tcat.sda.catalog.InvalidDataException;
 import edu.tamu.tcat.sda.catalog.NoSuchCatalogRecordException;
 import edu.tamu.tcat.sda.catalog.solr.WorksController;
 import edu.tamu.tcat.sda.catalog.works.EditWorkCommand;
@@ -32,9 +38,13 @@ import edu.tamu.tcat.sda.catalog.works.dv.WorkDV;
 @Path("/works")
 public class WorksResource
 {
-
+   private static final Logger logger = Logger.getLogger(WorksResource.class.getName());
 //   private ConfigurationProperties properties;
    private WorkRepository repo;
+
+   public WorksResource()
+   {
+   }
 
    // called by DS
    public void setConfigurationProperties(ConfigurationProperties properties)
@@ -58,9 +68,6 @@ public class WorksResource
    {
    }
 
-   public WorksResource()
-   {
-   }
 
    @GET
    @Produces(MediaType.APPLICATION_JSON)
@@ -88,12 +95,35 @@ public class WorksResource
    @Path("{workid}")
    @Consumes(MediaType.APPLICATION_JSON)
    @Produces(MediaType.APPLICATION_JSON)
-   public CustomResultsDV updateWork(@PathParam(value = "workid") String workId,
-                            WorkDV work) throws NoSuchCatalogRecordException, InterruptedException, ExecutionException
+   public CustomResultsDV updateWork(@PathParam(value = "workid") String workId, WorkDV work)
    {
-      EditWorkCommand workCommand = repo.edit(workId);
-      workCommand.setAll(work);
-      return new CustomResultsDV(workCommand.execute().get());
+      try
+      {
+         EditWorkCommand workCommand = repo.edit(workId);
+         workCommand.setAll(work);
+         String resultId = workCommand.execute().get();
+         return new CustomResultsDV(resultId);
+      }
+      catch (NoSuchCatalogRecordException ex)
+      {
+         String msg = "Cannot update bibliographic entry [" + workId + "]. The identified entry does not exist.";
+         logger.log(Level.WARNING, msg, ex);
+         throw new NotFoundException(msg);
+      }
+      catch (InvalidDataException ex)
+      {
+         String msg = "Cannot update bibliographic entry [" + workId + "]. The supplied data is invalid: " + ex.getMessage();
+         logger.log(Level.WARNING, msg, ex);
+         throw new BadRequestException(msg);
+      }
+
+      catch (Exception ex)
+      {
+         String msg = "Cannot update bibliographic entry [" + workId + "]. An internal error occurred. Please see server log for full details.";
+         logger.log(Level.SEVERE, msg, ex);
+         throw new InternalServerErrorException(msg);
+
+      }
    }
 
    @GET
