432c0919f7a5b13054ad92c69f8df893fe761b89
Neal Audenaert <nealaudenaert@tees.tamu.edu>
2015-01-29 14:05:28 -0600

    Created stub proxy class for solr documents.
    https://issues.citd.tamu.edu/browse/RI-6

---
+++ bundles/edu.tamu.tcat.sda.catalog.psql/src/edu/tamu/tcat/sda/catalog/relationships/search/solr/RelnSolrProxy.java
@@ -0,0 +1,25 @@
+package edu.tamu.tcat.sda.catalog.relationships.search.solr;
+
+import edu.tamu.tcat.sda.catalog.relationship.Relationship;
+
+/**
+ *  A data structure for representing the searchable fields associated with a {@link Relationship}.
+ */
+public class RelnSolrProxy
+{
+
+   // NOTE this is internal to the Solr search service. Probably/possibly add helper methods to retrieve
+   //      the reln (using the repo owned by the service) and other data structures as needed.
+
+
+   public RelnSolrProxy create(Relationship reln)
+   {
+      throw new UnsupportedOperationException();
+   }
+
+   public RelnSolrProxy()
+   {
+      // TODO Auto-generated constructor stub
+   }
+
+}
