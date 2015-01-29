3b5bcec833000a78af400cb9f2826e5393b31e08
Neal Audenaert <nealaudenaert@tees.tamu.edu>
2015-01-29 14:05:09 -0600

    Read Solr connection form config properties
    https://issues.citd.tamu.edu/browse/RI-6

--- bundles/edu.tamu.tcat.sda.catalog.psql/src/edu/tamu/tcat/sda/catalog/relationships/search/solr/SolrRelationshipSearchService.java
+++ bundles/edu.tamu.tcat.sda.catalog.psql/src/edu/tamu/tcat/sda/catalog/relationships/search/solr/SolrRelationshipSearchService.java
@@ -4,18 +4,35 @@ import java.net.URI;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+
+import edu.tamu.tcat.osgi.config.ConfigurationProperties;
 import edu.tamu.tcat.sda.catalog.relationship.Relationship;
 import edu.tamu.tcat.sda.catalog.relationship.RelationshipChangeEvent;
 import edu.tamu.tcat.sda.catalog.relationship.RelationshipRepository;
 import edu.tamu.tcat.sda.catalog.relationship.RelationshipSearchIndexManager;
 import edu.tamu.tcat.sda.catalog.relationship.RelationshipSearchService;
 
+/**
+ *  TODO include documentation about expected fields and format of the solr core.
+ *
+ */
 public class SolrRelationshipSearchService implements RelationshipSearchIndexManager, RelationshipSearchService
 {
+   /** Configuration property key that defines the URI for the Solr server. */
+   public static final String SOLR_API_ENDPOINT = "solr.api.endpoint";
+
+   /** Configuration property key that defines Solr core to be used for relationships. */
+   public static final String SOLR_CORE = "catalogentries.relationships.solr.core";
+
    private final static Logger logger = Logger.getLogger(SolrRelationshipSearchService.class.getName());
 
    private RelationshipRepository repo;
    private AutoCloseable registration;
+
+   private SolrServer solr;
+   private ConfigurationProperties config;
 
    public SolrRelationshipSearchService()
    {
@@ -26,15 +43,36 @@ public class SolrRelationshipSearchService implements RelationshipSearchIndexMan
       this.repo = repo;
    }
 
+   public void setConfiguration(ConfigurationProperties config)
+   {
+      this.config = config;
+   }
+
    public void activate()
    {
       logger.fine("Activating SolrRelationshipSearchService");
       registration = repo.addUpdateListener(this::onUpdate);
+
+      // construct Solr core
+      URI solrBaseUri = config.getPropertyValue(SOLR_API_ENDPOINT, URI.class);
+      String solrCore = config.getPropertyValue(SOLR_CORE, String.class);
+
+      URI coreUri = solrBaseUri.resolve(solrCore);
+      logger.info("Connecting to Solr Service [" + coreUri + "]");
+
+      solr = new HttpSolrServer(coreUri.toString());
    }
 
    public void deactivate()
    {
-      logger.fine("Deactivating SolrRelationshipSearchService");
+      logger.info("Deactivating SolrRelationshipSearchService");
+
+      unregisterRepoListener();
+      releaseSolrConnection();
+   }
+
+   private void unregisterRepoListener()
+   {
       if (registration != null)
       {
          try
@@ -47,6 +85,22 @@ public class SolrRelationshipSearchService implements RelationshipSearchIndexMan
          }
          finally {
             registration = null;
+         }
+      }
+   }
+
+   private void releaseSolrConnection()
+   {
+      logger.fine("Releasing connection to Solr server");
+      if (solr != null)
+      {
+         try
+         {
+            solr.shutdown();
+         }
+         catch (Exception e)
+         {
+            logger.log(Level.WARNING, "Failed to cleanly shut down connection to Solr server.", e);
          }
       }
    }
