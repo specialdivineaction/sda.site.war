a4e50946e04b162eabb093630477e93958189518
Neal Audenaert <nealaudenaert@tees.tamu.edu>
2015-02-06 11:13:33 -0600

    Added JavaDoc
    https://issues.citd.tamu.edu/browse/HT-1

--- bundles/edu.tamu.tcat.sda.catalog.relationship/src/edu/tamu/tcat/sda/catalog/relationship/RelationshipTypeRegistry.java
+++ bundles/edu.tamu.tcat.sda.catalog.relationship/src/edu/tamu/tcat/sda/catalog/relationship/RelationshipTypeRegistry.java
@@ -1,6 +1,19 @@
 package edu.tamu.tcat.sda.catalog.relationship;
 
+/**
+ *  A service to access defined relationship types.
+ */
 public interface RelationshipTypeRegistry
 {
+   /**
+    * Attempts to find and return the {@link RelationshipType} with the supplied
+    * identifier.
+    *
+    * @param typeIdentifier The unique identifer of the {@code RelationshipType}
+    *       to be retrieved.
+    * @return The identified {@code RelationshipType}
+    * @throws RelationshipException If the identified relationship type has not been
+    *       registered with this registry.
+    */
    RelationshipType resolve(String typeIdentifier) throws RelationshipException;
 }
