0c6f5cdc7a3f4a06ee58e0fc2eeebc9ec5d33839
Neal Audenaert <nealaudenaert@tees.tamu.edu>
2015-02-16 11:27:45 -0600

    Added query params for GET method
    https://issues.citd.tamu.edu/browse/RI-6

--- bundles/edu.tamu.tcat.sda.catalog.relationship.rest/src/edu/tamu/tcat/sda/catalog/relationship/rest/v1/RelationshipsCollectionService.java
+++ bundles/edu.tamu.tcat.sda.catalog.relationship.rest/src/edu/tamu/tcat/sda/catalog/relationship/rest/v1/RelationshipsCollectionService.java
@@ -5,12 +5,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+
+import com.google.common.base.Joiner;
 
 import edu.tamu.tcat.sda.catalog.relationship.EditRelationshipCommand;
 import edu.tamu.tcat.sda.catalog.relationship.Relationship;
@@ -26,6 +30,11 @@ public class RelationshipsCollectionService
 
    private RelationshipRepository repo;
    private RelationshipSearchService service;
+
+   public enum RelnDirection
+   {
+      from, to, any;
+   }
 
    public void setRepository(RelationshipRepository repo)
    {
@@ -50,16 +59,37 @@ public class RelationshipsCollectionService
    // /relationships?entity=<uri>[&type=<type_id>][&direction=from|to|any]
    @GET
    @Produces(MediaType.APPLICATION_JSON)
-   public List<RelationshipDV> getRelationships()
+   public List<RelationshipDV> getRelationships(
+         @QueryParam(value="entity") URI entity,
+         @QueryParam(value="type") String type,
+         @QueryParam(value="direction") String d)
    {
+      RelnDirection direction = parseDirection(d);
+
       // TODO - This will be taken care of on jira ticket https://issues.citd.tamu.edu/browse/RI-6
       List<RelationshipDV> relnDV = new ArrayList<>();
-      Iterable<Relationship> foundRelns = service.findRelationshipsFor(URI.create("works/2"));
+      Iterable<Relationship> foundRelns = service.findRelationshipsFor(entity);
       for (Relationship reln : foundRelns)
       {
-         relnDV.add(RelationshipDV.create(reln));
+           relnDV.add(RelationshipDV.create(reln));
       }
       return relnDV;
+   }
+
+   private RelnDirection parseDirection(String d)
+   {
+      if (d == null)
+         return RelnDirection.any;
+
+      try
+      {
+         return RelnDirection.valueOf(d.toLowerCase());
+      }
+      catch (IllegalArgumentException iea)
+      {
+         Joiner joiner = Joiner.on(", ");
+         throw new BadRequestException("Invalid value for query parameter 'direction' [" + d + "]. Must be one of the following: " + joiner.join(RelnDirection.values()));
+      }
    }
 
    @POST
