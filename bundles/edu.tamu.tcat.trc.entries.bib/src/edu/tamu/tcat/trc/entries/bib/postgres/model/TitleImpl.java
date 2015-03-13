7c7a559fae0cfb3b333f5e9bec831bb6209862cc
Matthew Barry <mbarry@tamu.edu>
2015-03-13 14:54:34 -0500

    implement hashCode() and equals() on TitleImpl class

--- bundles/edu.tamu.tcat.trc.entries.bib/src/edu/tamu/tcat/trc/entries/bib/postgres/model/TitleImpl.java
+++ bundles/edu.tamu.tcat.trc.entries.bib/src/edu/tamu/tcat/trc/entries/bib/postgres/model/TitleImpl.java
@@ -1,26 +1,35 @@
 package edu.tamu.tcat.trc.entries.bib.postgres.model;
 
+import java.util.Objects;
+
 import edu.tamu.tcat.trc.entries.bib.Title;
 import edu.tamu.tcat.trc.entries.bib.dto.TitleDV;
 
 public class TitleImpl implements Title
 {
-   private final TitleDV title;
+   private final String title;
+   private final String subTitle;
+   private final String type;
+   private final String language;
 
    public TitleImpl(TitleDV titleDV)
    {
-      this.title = titleDV;
+      title = titleDV.title;
+      subTitle = titleDV.subtitle;
+      type = titleDV.type;
+      language = titleDV.lg;
    }
+
    @Override
    public String getTitle()
    {
-      return title.title;
+      return this.title;
    }
 
    @Override
    public String getSubTitle()
    {
-      return title.subtitle;
+      return this.subTitle;
    }
 
    @Override
@@ -28,23 +37,59 @@ public class TitleImpl implements Title
    {
       StringBuilder sb = new StringBuilder();
 
-      sb.append(title.title);
+      sb.append(this.title);
 
-      if (title.subtitle != null && !title.subtitle.trim().isEmpty()) {
-         sb.append(": ").append(title.subtitle);
+      if (this.subTitle != null && !this.subTitle.trim().isEmpty()) {
+         sb.append(": ").append(this.subTitle);
       }
 
       return sb.toString();
    }
+
    @Override
    public String getType()
    {
-      return title.type;
+      return this.type;
    }
+
    @Override
    public String getLanguage()
    {
-      return title.lg;
+      return this.language;
+   }
+
+   @Override
+   public String toString()
+   {
+      return getFullTitle();
+   }
+
+   @Override
+   public int hashCode()
+   {
+      int result = 17;
+
+      result = 37 * result + (title == null ? 0 : title.hashCode());
+      result = 37 * result + (subTitle == null ? 0 : subTitle.hashCode());
+      result = 37 * result + (type == null ? 0 : type.hashCode());
+      result = 37 * result + (language == null ? 0 : language.hashCode());
+
+      return result;
+   }
+
+   @Override
+   public boolean equals(Object obj)
+   {
+      if (!(obj instanceof Title)) {
+         return false;
+      }
+
+      Title t = (Title)obj;
+
+      return Objects.equals(t.getTitle(), title) &&
+            Objects.equals(t.getSubTitle(), subTitle) &&
+            Objects.equals(t.getType(), type) &&
+            Objects.equals(t.getLanguage(), language);
    }
 
 }
