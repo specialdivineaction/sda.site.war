c997777be8b12d1809c1bcd752a49def71e9d823
Matthew Barry <mbarry@tamu.edu>
2016-03-31 15:54:28 -0500

    implement work CSV export endpoint

---
+++ bundles/edu.tamu.tcat.sda.catalog.rest/src/edu/tamu/tcat/sda/catalog/rest/WorkList.java
@@ -0,0 +1,289 @@
+package edu.tamu.tcat.sda.catalog.rest;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.StreamingOutput;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import edu.tamu.tcat.sda.catalog.rest.export.csv.CsvExporter;
+import edu.tamu.tcat.trc.entries.common.DateDescription;
+import edu.tamu.tcat.trc.entries.types.biblio.Edition;
+import edu.tamu.tcat.trc.entries.types.biblio.PublicationInfo;
+import edu.tamu.tcat.trc.entries.types.biblio.TitleDefinition;
+import edu.tamu.tcat.trc.entries.types.biblio.Volume;
+import edu.tamu.tcat.trc.entries.types.biblio.Work;
+import edu.tamu.tcat.trc.entries.types.biblio.copies.CopyReference;
+import edu.tamu.tcat.trc.entries.types.biblio.copies.repo.CopyReferenceRepository;
+import edu.tamu.tcat.trc.entries.types.biblio.repo.WorkRepository;
+
+@Path("/export/works")
+public class WorkList
+{
+   private static final Logger logger = Logger.getLogger(WorkList.class.getName());
+
+   private WorkRepository workRepo;
+   protected CopyReferenceRepository copyRepo;
+
+   private static final List<String> csvHeaders = Arrays.asList(
+         "type",
+         "workId",
+         "editionId",
+         "volumeId",
+         "shortTitle",
+         "canonicalTitle",
+         "bibliographicTitle",
+         "dateDisplay",
+         "dateIso",
+         "publisher",
+         "place",
+         "summary",
+         "hasDigitalCopy",
+         "authors");
+
+   public void setRepository(WorkRepository repo)
+   {
+      this.workRepo = repo;
+   }
+
+   public void activate()
+   {
+      Objects.requireNonNull(workRepo, "No work repository provided.");
+      Objects.requireNonNull(copyRepo, "no copy ref repository provided.");
+   }
+
+   private void doWrite(Writer writer) throws WebApplicationException
+   {
+      try
+      {
+         CsvExporter<WorkCsvRecord, WorkCsvRecord> exporter = new CsvExporter<>(o -> o, WorkCsvRecord.class);
+
+         writer.write(String.join(", ", csvHeaders));
+         writer.write(System.lineSeparator());
+
+         Iterator<WorkCsvRecord> iterator = StreamSupport.stream(workRepo.listWorks().spliterator(), false)
+            .flatMap(this::stream)
+            .iterator();
+
+         exporter.export(iterator, writer);
+         writer.flush();
+      }
+      catch (IOException e)
+      {
+         // NOTE: various clients may abort connection before file download completes, resulting in this error
+         logger.log(Level.FINE, "Failed to export author list. Unable to read data from repository.", e);
+         Response resp = Response.serverError()
+               .entity("Cannot complete author list export. Likely caused by closed connection.")
+               .build();
+         throw new WebApplicationException(resp);
+      }
+      catch (Exception e)
+      {
+         logger.log(Level.SEVERE, "Failed to export author list. Unexpected error.", e);
+         Response resp = Response.serverError()
+               .entity("Cannot export author list. See server logs for details.")
+               .build();
+         throw new WebApplicationException(resp);
+      }
+   }
+
+   @GET
+   @Produces("text/csv; charset=UTF-8")
+   public Response getBasicWorkListCSV()
+   {
+      StreamingOutput stream = os -> {
+         Writer writer = new BufferedWriter(new OutputStreamWriter(os));
+
+         // HACK: fork for background thread to do CSV export using Jackson b/c if it's done in
+         //       Jersey/REST call stack, it will get the wrong version of Jackson via different
+         //       OSGi class loaders. Fork forces Jackson classes to be loaded from this class's
+         //       class loader instead of Jersey's.
+         ExecutorService svc = Executors.newSingleThreadExecutor();
+         Future<?> future = svc.submit(() -> doWrite(writer));
+         try
+         {
+            future.get(5, TimeUnit.MINUTES);
+         }
+         catch (Exception e)
+         {
+            throw new WebApplicationException(e, Status.INTERNAL_SERVER_ERROR);
+         }
+         finally
+         {
+            svc.shutdown();
+         }
+      };
+
+      return Response.ok(stream)
+            .header("Content-Disposition", "inline; filename=\"works.csv\"")
+            .build();
+   }
+
+
+   private Stream<WorkCsvRecord> stream(Work work)
+   {
+      WorkCsvRecord workRecord = createRecord(work);
+      Stream<WorkCsvRecord> editionRecords = work.getEditions().stream()
+            .flatMap(edition -> stream(edition, work.getId()));
+
+      return Stream.concat(Stream.of(workRecord), editionRecords);
+   }
+
+   private Stream<WorkCsvRecord> stream(Edition edition, String workId)
+   {
+      WorkCsvRecord editionRecord = create(edition, workId);
+      Stream<WorkCsvRecord> volumeRecords = edition.getVolumes().stream()
+            .map(volume -> create(volume, edition.getId(), workId));
+
+      return Stream.concat(Stream.of(editionRecord), volumeRecords);
+   }
+
+   private WorkCsvRecord createRecord(Work work)
+   {
+      WorkCsvRecord record = new WorkCsvRecord();
+      record.type = "work";
+      record.workId = work.getId();
+
+      TitleDefinition workTitle = work.getTitle();
+      record.canonicalTitle = workTitle.getShortTitle().getFullTitle();
+      record.bibliographicTitle = workTitle.getCanonicalTitle().getFullTitle();
+
+      record.summary = work.getSummary();
+
+      URI workUri = URI.create("works/" + work.getId());
+      List<CopyReference> copies = copyRepo.getCopies(workUri, false);
+      record.hasDigitalCopy = copies.isEmpty() ? "false" : "true";
+
+      String[] authors = (String[])StreamSupport.stream(work.getAuthors().spliterator(), false)
+            .map(ref -> ref.getFirstName() + " " + ref.getLastName())
+            .toArray();
+      record.authors = String.join(";", authors);
+
+      return record;
+   }
+
+   private WorkCsvRecord create(Edition edition, String workId)
+   {
+      WorkCsvRecord record = new WorkCsvRecord();
+      record.type = "edition";
+      record.workId = workId;
+      record.editionId = edition.getId();
+
+      PublicationInfo publicationInfo = edition.getPublicationInfo();
+      record.publisher = publicationInfo.getPublisher();
+      record.place = publicationInfo.getLocation();
+
+      DateDescription publicationDate = publicationInfo.getPublicationDate();
+      record.dateDisplay = publicationDate.getDescription();
+      record.dateIso = publicationDate.getCalendar().toString();
+
+      record.canonicalTitle = edition.getTitles().stream()
+            .filter(title -> title.getType().equalsIgnoreCase("short"))
+            .map(title -> title.getFullTitle())
+            .findFirst()
+            .orElse(null);
+
+      record.bibliographicTitle = edition.getTitles().stream()
+            .filter(title -> title.getType().equalsIgnoreCase("canonical"))
+            .map(title -> title.getFullTitle())
+            .findFirst()
+            .orElse(null);
+
+      record.summary = edition.getSummary();
+
+      URI editionUri = URI.create("works/" + workId + "/editions/" + edition.getId());
+      List<CopyReference> copies = copyRepo.getCopies(editionUri, false);
+      record.hasDigitalCopy = copies.isEmpty() ? "false" : "true";
+
+      String[] authors = (String[])edition.getAuthors().stream()
+            .map(ref -> ref.getFirstName() + " " + ref.getLastName())
+            .toArray();
+      record.authors = String.join(";", authors);
+
+      return record;
+   }
+
+   private WorkCsvRecord create(Volume volume, String editionId, String workId)
+   {
+      WorkCsvRecord record = new WorkCsvRecord();
+      record.type = "volume";
+      record.workId = workId;
+      record.editionId = editionId;
+      record.volumeId = volume.getId();
+
+      PublicationInfo publicationInfo = volume.getPublicationInfo();
+      record.publisher = publicationInfo.getPublisher();
+      record.place = publicationInfo.getLocation();
+
+      DateDescription publicationDate = publicationInfo.getPublicationDate();
+      record.dateDisplay = publicationDate.getDescription();
+      record.dateIso = publicationDate.getCalendar().toString();
+
+      record.canonicalTitle = volume.getTitles().stream()
+            .filter(title -> title.getType().equalsIgnoreCase("short"))
+            .map(title -> title.getFullTitle())
+            .findFirst()
+            .orElse(null);
+
+      record.bibliographicTitle = volume.getTitles().stream()
+            .filter(title -> title.getType().equalsIgnoreCase("canonical"))
+            .map(title -> title.getFullTitle())
+            .findFirst()
+            .orElse(null);
+
+      record.summary = volume.getSummary();
+
+      URI editionUri = URI.create("works/" + workId + "/editions/" + editionId + "/volumes/" + volume.getId());
+      List<CopyReference> copies = copyRepo.getCopies(editionUri, false);
+      record.hasDigitalCopy = copies.isEmpty() ? "false" : "true";
+
+      String[] authors = (String[])volume.getAuthors().stream()
+            .map(ref -> ref.getFirstName() + " " + ref.getLastName())
+            .toArray();
+      record.authors = String.join(";", authors);
+
+      return record;
+   }
+
+
+   @JsonPropertyOrder
+   public class WorkCsvRecord
+   {
+      public String type;
+      public String workId;
+      public String editionId;
+      public String volumeId;
+      public String shortTitle;
+      public String canonicalTitle;
+      public String bibliographicTitle;
+      public String dateDisplay;
+      public String dateIso;
+      public String publisher;
+      public String place;
+      public String summary;
+      public String hasDigitalCopy;
+      public String authors;
+   }
+}
