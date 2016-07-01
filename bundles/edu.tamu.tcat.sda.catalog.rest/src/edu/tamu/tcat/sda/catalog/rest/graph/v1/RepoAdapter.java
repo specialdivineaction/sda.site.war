84514d3bfcdd417cec69e73389f6d8ca6b86f531
Matthew Barry <mbarry@tamu.edu>
2016-07-01 08:49:40 -0500

    implement works rollup graph REST API

---
+++ bundles/edu.tamu.tcat.sda.catalog.rest/src/edu/tamu/tcat/sda/catalog/rest/graph/v1/RepoAdapter.java
@@ -0,0 +1,172 @@
+package edu.tamu.tcat.sda.catalog.rest.graph.v1;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import edu.tamu.tcat.sda.catalog.rest.graph.GraphDTO;
+import edu.tamu.tcat.trc.entries.types.biblio.Title;
+import edu.tamu.tcat.trc.entries.types.biblio.TitleDefinition;
+import edu.tamu.tcat.trc.entries.types.biblio.Work;
+import edu.tamu.tcat.trc.entries.types.reln.Anchor;
+import edu.tamu.tcat.trc.entries.types.reln.AnchorSet;
+import edu.tamu.tcat.trc.entries.types.reln.Relationship;
+import edu.tamu.tcat.trc.entries.types.reln.RelationshipType;
+
+public class RepoAdapter
+{
+   private static final Pattern workIdPattern = Pattern.compile("^works/([^/]+)");
+
+   public static GraphDTO.Node toDTO(Work work)
+   {
+      GraphDTO.Node dto = new GraphDTO.Node();
+
+      dto.id = work.getId();
+      dto.label = extractTitle(work.getTitle());
+      dto.type = "work";
+
+      return dto;
+   }
+
+   /**
+    * Since relationships can relate potentially many works, this method can produce multiple edges from a single relationship.
+    * @param reln
+    * @return
+    */
+   public static List<GraphDTO.Edge> toDTO(Relationship reln)
+   {
+      RelationshipType type = reln.getType();
+
+      AnchorSet relatedSet = reln.getRelatedEntities();
+      Collection<Anchor> relateds = relatedSet == null ? null : relatedSet.getAnchors();
+
+      AnchorSet targetSet = reln.getTargetEntities();
+      Collection<Anchor> targets = targetSet == null ? null : targetSet.getAnchors();
+
+      List<GraphDTO.Edge> edges = new ArrayList<>();
+
+      // HACK Desired structure of undirected relns lists related works within the relatedEntities anchor set
+      //      Existing data does not have this structure, but follows the related -> target paradigm of directed relns.
+      if (!type.isDirected() && relateds != null && relateds.size() > 1 && (targets == null || targets.isEmpty()))
+      {
+         // for every related entity, create an edge to every other entity (excluding self)
+         for (Anchor source : relateds)
+         {
+            for (Anchor target : relateds)
+            {
+               if (source == target)
+               {
+                  continue;
+               }
+
+               GraphDTO.Edge dto = createEdge(source, target, reln);
+               if (dto != null)
+               {
+                  edges.add(dto);
+               }
+
+            }
+         }
+      }
+      else
+      {
+         for (Anchor source : relateds)
+         {
+            for (Anchor target : targets)
+            {
+               GraphDTO.Edge dto = createEdge(source, target, reln);
+               if (dto != null)
+               {
+                  edges.add(dto);
+               }
+            }
+         }
+      }
+
+      return edges;
+   }
+
+   /**
+    * Creates a new edge from the given source and target anchors with metadata from the given relationship.
+    * This method assumes that the source and target anchors are both contained within the given relationship.
+    * @param source
+    * @param target
+    * @param reln Provides edge metadata
+    * @return
+    */
+   private static GraphDTO.Edge createEdge(Anchor source, Anchor target, Relationship reln)
+   {
+
+      Collection<URI> sourceUris = source.getEntryIds();
+      Collection<URI> targetUris = target.getEntryIds();
+
+      if (sourceUris == null || sourceUris.isEmpty() || targetUris == null || targetUris.isEmpty())
+      {
+         return null;
+      }
+
+
+      // HACK use first anchor entry URI
+      String sourceUri = sourceUris.iterator().next().toString();
+      String targetUri = targetUris.iterator().next().toString();
+
+      // extract work ID
+      Matcher sourceWorkIdMatcher = workIdPattern.matcher(sourceUri);
+      Matcher targetWorkIdMatcher = workIdPattern.matcher(targetUri);
+      if (!sourceWorkIdMatcher.matches() || !targetWorkIdMatcher.matches()) {
+         return null;
+      }
+
+      GraphDTO.Edge dto = new GraphDTO.Edge();
+      dto.source = sourceWorkIdMatcher.group(1);
+      dto.target = targetWorkIdMatcher.group(1);
+
+      RelationshipType type = reln.getType();
+      dto.directed = type.isDirected(); // obviously undirected at this point due to conditional
+      dto.relation = type.getIdentifier();
+      dto.label = type.getTitle();
+
+      dto.metadata = new HashMap<>();
+      dto.metadata.put("relationshipId", reln.getId());
+      dto.metadata.put("description", reln.getDescription());
+
+      return dto;
+   }
+
+   /**
+    * Attempt to pull a title from the given work, or {@code null} if no title can be found.
+    * @param titleDefinition
+    * @return
+    */
+   private static String extractTitle(TitleDefinition titleDefinition)
+   {
+      if (titleDefinition == null)
+      {
+         return null;
+      }
+
+      Title shortTitle = titleDefinition.get("short");
+      if (shortTitle != null)
+      {
+         return shortTitle.getFullTitle();
+      }
+
+      Title canonicalTitle = titleDefinition.get("canonical");
+      if (canonicalTitle != null)
+      {
+         return canonicalTitle.getFullTitle();
+      }
+
+      Title bibliographicTitle = titleDefinition.get("bibliographic");
+      if (bibliographicTitle != null)
+      {
+         return bibliographicTitle.getFullTitle();
+      }
+
+      return null;
+   }
+}
---
+++ bundles/edu.tamu.tcat.sda.catalog.rest/src/edu/tamu/tcat/sda/catalog/rest/graph/v1/WorkGraphResource.java
@@ -0,0 +1,91 @@
+package edu.tamu.tcat.sda.catalog.rest.graph.v1;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import edu.tamu.tcat.sda.catalog.rest.graph.GraphDTO;
+import edu.tamu.tcat.trc.entries.repo.CatalogRepoException;
+import edu.tamu.tcat.trc.entries.types.biblio.Work;
+import edu.tamu.tcat.trc.entries.types.biblio.repo.WorkRepository;
+import edu.tamu.tcat.trc.entries.types.reln.Relationship;
+import edu.tamu.tcat.trc.entries.types.reln.repo.RelationshipRepository;
+import edu.tamu.tcat.trc.entries.types.reln.search.RelationshipDirection;
+import edu.tamu.tcat.trc.entries.types.reln.search.RelationshipQueryCommand;
+import edu.tamu.tcat.trc.entries.types.reln.search.RelationshipSearchResult;
+import edu.tamu.tcat.trc.entries.types.reln.search.RelationshipSearchService;
+import edu.tamu.tcat.trc.entries.types.reln.search.RelnSearchProxy;
+import edu.tamu.tcat.trc.search.SearchException;
+
+public class WorkGraphResource
+{
+   private final WorkRepository workRepo;
+   private final RelationshipSearchService relnSearchService;
+   private final RelationshipRepository relnRepo;
+
+   public WorkGraphResource(WorkRepository workRepo, RelationshipSearchService relnSearchService, RelationshipRepository relnRepo)
+   {
+      this.workRepo = workRepo;
+      this.relnSearchService = relnSearchService;
+      this.relnRepo = relnRepo;
+   }
+
+   @GET
+   @Produces(MediaType.APPLICATION_JSON)
+   public GraphDTO.SingleGraph getGraph()
+   {
+      Iterable<Work> works = () -> workRepo.getAllWorks();
+
+      GraphDTO.Graph graph = new GraphDTO.Graph();
+
+      graph.type = "work-reln";
+      graph.nodes = StreamSupport.stream(works.spliterator(), false)
+         .map(RepoAdapter::toDTO)
+         .collect(Collectors.toList());
+
+      graph.edges = graph.nodes.stream()
+         .flatMap(node -> findRelationships(node.id, RelationshipDirection.any))
+         .flatMap(reln -> RepoAdapter.toDTO(reln).stream())
+         .collect(Collectors.toList());
+
+      return GraphDTO.SingleGraph.create(graph);
+   }
+
+   private Stream<Relationship> findRelationships(String workId, RelationshipDirection direction)
+   {
+      try
+      {
+         RelationshipQueryCommand command = relnSearchService.createQueryCommand();
+         command.forEntity(URI.create("works/" + workId), direction);
+         RelationshipSearchResult result = command.execute();
+         List<RelnSearchProxy> relationships = result.get();
+         return relationships.stream()
+            .map(proxy -> proxy.id)
+            .map(this::lookupRelationship)
+            .filter(Objects::nonNull);
+      }
+      catch (SearchException e)
+      {
+         return Stream.empty();
+      }
+   }
+
+   private Relationship lookupRelationship(String relnId)
+   {
+      try
+      {
+         return relnRepo.get(relnId);
+      }
+      catch (CatalogRepoException e)
+      {
+         return null;
+      }
+   }
+}
