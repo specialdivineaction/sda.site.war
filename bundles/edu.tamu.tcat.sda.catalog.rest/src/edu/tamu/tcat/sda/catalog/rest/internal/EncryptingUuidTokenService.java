0a5758957a7914917c6d97232e65ba6776a3e512
Matthew Barry <mbarry@tamu.edu>
2016-07-22 14:13:32 -0500

    update to auth 2.0

--- bundles/edu.tamu.tcat.sda.catalog.rest/src/edu/tamu/tcat/sda/catalog/rest/internal/EncryptingUuidTokenService.java
+++ bundles/edu.tamu.tcat.sda.catalog.rest/src/edu/tamu/tcat/sda/catalog/rest/internal/EncryptingUuidTokenService.java
@@ -3,12 +3,11 @@ package edu.tamu.tcat.sda.catalog.rest.internal;
 import java.nio.ByteBuffer;
 import java.time.Instant;
 import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.Base64;
 import java.util.UUID;
 
-import edu.tamu.tcat.account.token.AccountTokenException;
+import edu.tamu.tcat.account.AccountException;
 import edu.tamu.tcat.account.token.TokenService;
 import edu.tamu.tcat.crypto.CryptoProvider;
 import edu.tamu.tcat.crypto.SecureToken;
@@ -18,40 +17,40 @@ import edu.tamu.tcat.osgi.config.ConfigurationProperties;
 public class EncryptingUuidTokenService implements TokenService<UUID>
 {
    private static final String PROP_TOKEN_KEY = "token.key";
-   
+
    //TODO: remove these default values and require a config property to be set
    @Deprecated
    final String keyb64_128 = "blahDiddlyBlahSchmacko";
    @Deprecated
    final String keyb64_256 = "blahDiddlyBlahSchmackety+ABitLongerThanThat+";
-   
+
    private SecureToken secureToken;
-   
+
    private CryptoProvider crypto;
    private ConfigurationProperties props;
-   
+
    public void bind(CryptoProvider cp)
    {
       crypto = cp;
    }
-   
+
    public void bind(ConfigurationProperties cp)
    {
       props = cp;
    }
-   
-   public void activate() throws AccountTokenException
+
+   public void activate() throws AccountException
    {
       byte[] key;
       try
       {
          String encryptionKey = props.getPropertyValue(PROP_TOKEN_KEY, String.class, keyb64_128);
-         
+
          key = Base64.getDecoder().decode(encryptionKey);
       }
       catch (Exception e)
       {
-         throw new AccountTokenException("Could not decode token key", e);
+         throw new AccountException("Could not decode token key", e);
       }
       try
       {
@@ -59,12 +58,12 @@ public class EncryptingUuidTokenService implements TokenService<UUID>
       }
       catch (Exception e)
       {
-         throw new AccountTokenException("Could not construct secure token", e);
+         throw new AccountException("Could not construct secure token", e);
       }
    }
-   
+
    @Override
-   public TokenService.TokenData<UUID> createTokenData(UUID id) throws AccountTokenException
+   public TokenService.TokenData<UUID> createTokenData(UUID id) throws AccountException
    {
       ByteBuffer buffer = ByteBuffer.allocate(4 + 8 + 16);
       //HACK: allow configuration of the expiration duration
@@ -78,57 +77,55 @@ public class EncryptingUuidTokenService implements TokenService<UUID>
       try
       {
          String stok = secureToken.getToken(buffer);
-         String exp = DateTimeFormatter.ISO_ZONED_DATE_TIME.format(expires);
-         return new UuidTokenData(stok, id, exp);
+         return new UuidTokenData(stok, id, expires);
       }
       catch (TokenException e)
       {
-         throw new AccountTokenException("Could not create token", e);
+         throw new AccountException("Could not create token", e);
       }
    }
-   
+
    @Override
-   public UUID unpackToken(String token) throws AccountTokenException
+   public UUID unpackToken(String token)
    {
       return UUID.fromString(token);
    }
-   
+
    @Override
    public Class<UUID> getPayloadType()
    {
       return UUID.class;
    }
-   
+
    static class UuidTokenData implements TokenService.TokenData<UUID>
    {
-      private String token;
-      private String expireStr;
-      private UUID uuid;
+      private final String token;
+      private final UUID uuid;
+      private final ZonedDateTime expiration;
 
-      public UuidTokenData(String t, UUID uuid, String expStr)
+      public UuidTokenData(String token, UUID uuid, ZonedDateTime expiration)
       {
-         token = t;
+         this.token = token;
          this.uuid = uuid;
-         expireStr = expStr;
+         this.expiration = expiration;
       }
-      
+
       @Override
       public String getToken()
       {
          return token;
       }
-      
+
       @Override
       public UUID getPayload()
       {
          return uuid;
       }
 
-      @Deprecated
       @Override
-      public String getExpireStr()
+      public ZonedDateTime getExpiration()
       {
-         return expireStr;
+         return expiration;
       }
    }
 }
