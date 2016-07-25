04066b44edd2bd9764fe8c9307b73d7ca3d1a489
Matthew Barry <mbarry@tamu.edu>
2016-07-25 12:27:30 -0500

    expose people rollup resource from graph API service

--- bundles/edu.tamu.tcat.sda.catalog.rest/src/edu/tamu/tcat/sda/catalog/rest/graph/GraphResourceService.java
+++ bundles/edu.tamu.tcat.sda.catalog.rest/src/edu/tamu/tcat/sda/catalog/rest/graph/GraphResourceService.java
@@ -6,28 +6,29 @@ import java.util.logging.Logger;
 
 import javax.ws.rs.Path;
 
+import edu.tamu.tcat.sda.catalog.rest.graph.v1.PeopleGraphResource;
 import edu.tamu.tcat.sda.catalog.rest.graph.v1.WorkGraphResource;
 import edu.tamu.tcat.trc.entries.types.biblio.repo.WorkRepository;
+import edu.tamu.tcat.trc.entries.types.bio.repo.PeopleRepository;
 import edu.tamu.tcat.trc.entries.types.reln.repo.RelationshipRepository;
-import edu.tamu.tcat.trc.entries.types.reln.search.RelationshipSearchService;
 
 @Path("graph")
 public class GraphResourceService
 {
    private static final Logger logger = Logger.getLogger(GraphResourceService.class.getName());
 
+   private PeopleRepository peopleRepo;
    private WorkRepository workRepo;
-   private RelationshipSearchService relnSearchService;
    private RelationshipRepository relnRepo;
+
+   public void setPeopleRepository(PeopleRepository peopleRepo)
+   {
+      this.peopleRepo = peopleRepo;
+   }
 
    public void setWorkRepository(WorkRepository workRepo)
    {
       this.workRepo = workRepo;
-   }
-
-   public void setRelationshipSearchService(RelationshipSearchService relnSearchService)
-   {
-      this.relnSearchService = relnSearchService;
    }
 
    public void setRelationshipRepository(RelationshipRepository relnRepo)
@@ -37,22 +38,28 @@ public class GraphResourceService
 
    public void activate()
    {
+      Objects.requireNonNull(peopleRepo, "People repository not provided");
       Objects.requireNonNull(workRepo, "Work repository not provided");
-      Objects.requireNonNull(relnSearchService, "Relationship search service not provided");
       Objects.requireNonNull(relnRepo, "Relationship repository not provided");
       logger.log(Level.INFO, "starting graph REST resource service");
    }
 
    public void dispose()
    {
+      peopleRepo = null;
       workRepo = null;
-      relnSearchService = null;
       relnRepo = null;
    }
 
    @Path("works")
    public WorkGraphResource rollupWorks()
    {
-      return new WorkGraphResource(workRepo, relnSearchService, relnRepo);
+      return new WorkGraphResource(workRepo, relnRepo);
+   }
+
+   @Path("people")
+   public PeopleGraphResource rollupPeople()
+   {
+      return new PeopleGraphResource(peopleRepo, workRepo, relnRepo);
    }
 }
