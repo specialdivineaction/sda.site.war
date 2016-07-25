e96bb8a7fbdfeaf7e7309f4fb522b63ae1f1aef1
Matthew Barry <mbarry@tamu.edu>
2016-07-25 12:25:57 -0500

    prevent duplicate edge entries from appearing in graph

--- bundles/edu.tamu.tcat.sda.catalog.rest/src/edu/tamu/tcat/sda/catalog/rest/graph/v1/WorkGraphResource.java
+++ bundles/edu.tamu.tcat.sda.catalog.rest/src/edu/tamu/tcat/sda/catalog/rest/graph/v1/WorkGraphResource.java
@@ -1,10 +1,7 @@
 package edu.tamu.tcat.sda.catalog.rest.graph.v1;
 
-import java.net.URI;
-import java.util.List;
-import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 import javax.ws.rs.GET;
@@ -16,23 +13,15 @@ import edu.tamu.tcat.trc.entries.types.biblio.Work;
 import edu.tamu.tcat.trc.entries.types.biblio.repo.WorkRepository;
 import edu.tamu.tcat.trc.entries.types.reln.Relationship;
 import edu.tamu.tcat.trc.entries.types.reln.repo.RelationshipRepository;
-import edu.tamu.tcat.trc.entries.types.reln.search.RelationshipDirection;
-import edu.tamu.tcat.trc.entries.types.reln.search.RelationshipQueryCommand;
-import edu.tamu.tcat.trc.entries.types.reln.search.RelationshipSearchResult;
-import edu.tamu.tcat.trc.entries.types.reln.search.RelationshipSearchService;
-import edu.tamu.tcat.trc.entries.types.reln.search.RelnSearchProxy;
-import edu.tamu.tcat.trc.search.SearchException;
 
 public class WorkGraphResource
 {
    private final WorkRepository workRepo;
-   private final RelationshipSearchService relnSearchService;
    private final RelationshipRepository relnRepo;
 
-   public WorkGraphResource(WorkRepository workRepo, RelationshipSearchService relnSearchService, RelationshipRepository relnRepo)
+   public WorkGraphResource(WorkRepository workRepo, RelationshipRepository relnRepo)
    {
       this.workRepo = workRepo;
-      this.relnSearchService = relnSearchService;
       this.relnRepo = relnRepo;
    }
 
@@ -51,30 +40,17 @@ public class WorkGraphResource
          .map(RepoAdapter::toDTO)
          .collect(Collectors.toList());
 
-      graph.edges = graph.nodes.stream()
-         .flatMap(node -> findRelationships(node.id, RelationshipDirection.any))
+      Set<String> nodeIds = graph.nodes.stream()
+         .map(node -> node.id)
+         .collect(Collectors.toSet());
+
+      Iterable<Relationship> relationships = () -> relnRepo.getAllRelationships();
+
+      graph.edges = StreamSupport.stream(relationships.spliterator(), true)
          .flatMap(reln -> RepoAdapter.toDTO(reln).stream())
+         .filter(edge -> nodeIds.contains(edge.source) && nodeIds.contains(edge.target))
          .collect(Collectors.toList());
 
       return GraphDTO.SingleGraph.create(graph);
-   }
-
-   private Stream<Relationship> findRelationships(String workId, RelationshipDirection direction)
-   {
-      try
-      {
-         RelationshipQueryCommand command = relnSearchService.createQueryCommand();
-         command.forEntity(URI.create("works/" + workId), direction);
-         RelationshipSearchResult result = command.execute();
-         List<RelnSearchProxy> relationships = result.get();
-         return relationships.stream()
-            .map(proxy -> proxy.id)
-            .map(relnRepo::get)
-            .filter(Objects::nonNull);
-      }
-      catch (SearchException e)
-      {
-         return Stream.empty();
-      }
    }
 }
